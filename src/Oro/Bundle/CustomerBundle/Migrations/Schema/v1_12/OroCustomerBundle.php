60786d14b3bae01e2c851a4c0637538dc3a355c9
Yehven Petrov <ypetrov@magecore.com>
2017-02-08 20:04:38 +0200

    BB-5073: System setting default_account_owner store object instead of id
    - fix save config of default customer

--- src/Oro/Bundle/CustomerBundle/Migrations/Schema/v1_12/OroCustomerBundle.php
+++ src/Oro/Bundle/CustomerBundle/Migrations/Schema/v1_12/OroCustomerBundle.php
@@ -5,6 +5,7 @@ namespace Oro\Bundle\CustomerBundle\Migrations\Schema\v1_12;
 use Doctrine\DBAL\Schema\Schema;
 
 use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\ParametrizedSqlMigrationQuery;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
 class OroCustomerBundle implements Migration
@@ -15,5 +16,19 @@ class OroCustomerBundle implements Migration
     public function up(Schema $schema, QueryBag $queries)
     {
         $schema->dropTable('oro_customer_user_org');
+        //remove invalid record because of error there is a NULL value
+        $this->removeFromConfig($queries, 'default_customer_owner');
+    }
+
+    /**
+     * @param QueryBag $queries
+     * @param string
+     */
+    protected function removeFromConfig(QueryBag $queries, $name)
+    {
+        $queries->addQuery(new ParametrizedSqlMigrationQuery(
+            'DELETE FROM oro_config_value WHERE name = :name AND section = :section AND text_value IS NULL',
+            ['name' => $name, 'section' => 'oro_customer']
+        ));
     }
 }
