8cb946bcdf68a71d26073840fe5f6b13f252d3e7
Dmitriy Pasechnik <dpasechnik@oroinc.com>
2017-02-21 17:30:24 +0200

    BB-7811: Write behat test for price sorting in system configuration

--- src/Oro/Bundle/CustomerBundle/Tests/Behat/Context/FeatureContext.php
+++ src/Oro/Bundle/CustomerBundle/Tests/Behat/Context/FeatureContext.php
@@ -14,35 +14,6 @@ use Oro\Bundle\UserBundle\Entity\Role;
 class FeatureContext extends BaseFeatureContext
 {
     /**
-     * Example: AmandaRCole@example.org customer user has Buyer role
-     *
-     * @Given /^(?P<username>\S+) customer user has (?P<role>[\w\s]+) role$/
-     *
-     * @param string $username
-     * @param string $role
-     */
-    public function customerUserHasRole($username, $role)
-    {
-        $customerUser = $this->getCustomerUser($username);
-        self::assertNotNull($customerUser, sprintf('Could not found customer user "%s",', $username));
-
-        $customerUserRole = null;
-
-        /** @var Role $item */
-        foreach ($customerUser->getRoles() as $item) {
-            if ($role === $item->getLabel()) {
-                $customerUserRole = $item;
-                break;
-            }
-        }
-
-        self::assertNotNull(
-            $customerUserRole,
-            sprintf('Customer user "%s" was found, but without role "%s"', $username, $role)
-        );
-    }
-
-    /**
      * @Then I should see that :priceListName price list is in :rowNum row on view page
      * @param string $priceListName
      * @param int $rowNum
@@ -58,26 +29,5 @@ class FeatureContext extends BaseFeatureContext
         $rows = $table->findAll('css', 'tr');
         self::assertNotEmpty($rows[$rowNum]);
         self::assertContains($priceListName, $rows[$rowNum]->getText());
-    }
-
-    /**
-     * @param string $username
-     * @return CustomerUser
-     */
-    protected function getCustomerUser($username)
-    {
-        return $this->getRepository(CustomerUser::class)->findOneBy(['username' => $username]);
-    }
-
-    /**
-     * @param string $className
-     * @return ObjectRepository
-     */
-    protected function getRepository($className)
-    {
-        return $this->getContainer()
-            ->get('doctrine')
-            ->getManagerForClass($className)
-            ->getRepository($className);
     }
 }
