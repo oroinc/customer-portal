f4085a7d0d099b7c5b79ac83f544e1df5c3d3a85
Mykola Sedletskyi <msedletskyi@oroinc.com>
2017-02-23 14:44:24 +0200

    BB-5664: Replace FOS rest controllers with delete actions to use ApiBundle  - Shopping list bundle delete shopping list button - Customer User bundle

--- src/Oro/Bundle/CustomerBundle/Tests/Functional/Controller/Api/Rest/CustomerGroupControllerTest.php
+++ src/Oro/Bundle/CustomerBundle/Tests/Functional/Controller/Api/Rest/CustomerGroupControllerTest.php
@@ -2,15 +2,14 @@
 
 namespace Oro\Bundle\CustomerBundle\Tests\Functional\Controller\Api\Rest;
 
-use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
 use Oro\Bundle\CustomerBundle\Entity\CustomerGroup;
-use Oro\Bundle\CustomerBundle\DependencyInjection\Configuration;
+use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
 
 class CustomerGroupControllerTest extends WebTestCase
 {
     protected function setUp()
     {
-        $this->initClient([], $this->generateWsseAuthHeader());
+        $this->initClient([], $this->generateBasicAuthHeader());
         $this->client->useHashNavigation(true);
         $this->loadFixtures(
             [
@@ -25,11 +24,21 @@ class CustomerGroupControllerTest extends WebTestCase
         $entity = $this->getReference('customer_group.group1');
         $id = $entity->getId();
         $this->client->request(
-            'DELETE',
-            $this->getUrl('oro_api_customer_delete_customer_group', ['id' => $id])
+            'GET',
+            $this->getUrl(
+                'oro_action_operation_execute',
+                [
+                    'operationName' => 'oro_customer_groups_delete',
+                    'entityId[id]' => $id,
+                    'entityClass' => $this->getContainer()->getParameter('oro_customer.entity.customer_group.class'),
+                ]
+            ),
+            [],
+            [],
+            ['HTTP_X_REQUESTED_WITH' => 'XMLHttpRequest']
         );
-        $result = $this->client->getResponse();
-        $this->assertEmptyResponseStatusCodeEquals($result, 204);
+
+        $this->assertJsonResponseStatusCodeEquals($this->client->getResponse(), 200);
     }
 
     public function testDeleteAnonymousUserGroup()
@@ -39,8 +48,18 @@ class CustomerGroupControllerTest extends WebTestCase
             ->get('oro_customer.anonymous_customer_group');
 
         $this->client->request(
-            'DELETE',
-            $this->getUrl('oro_api_customer_delete_customer_group', ['id' => $id])
+            'GET',
+            $this->getUrl(
+                'oro_action_operation_execute',
+                [
+                    'operationName' => 'oro_customer_groups_delete',
+                    'entityId[id]' => $id,
+                    'entityClass' => $this->getContainer()->getParameter('oro_customer.entity.customer_group.class'),
+                ]
+            ),
+            [],
+            [],
+            ['HTTP_X_REQUESTED_WITH' => 'XMLHttpRequest']
         );
         $result = $this->client->getResponse();
         $this->assertSame(403, $result->getStatusCode());
--- src/Oro/Bundle/CustomerBundle/Tests/Functional/Controller/Frontend/Api/Rest/CustomerUserControllerTest.php
+++ src/Oro/Bundle/CustomerBundle/Tests/Functional/Controller/Frontend/Api/Rest/CustomerUserControllerTest.php
@@ -24,31 +24,6 @@ class CustomerUserControllerTest extends WebTestCase
     }
 
     /**
-     * @dataProvider deleteDataProvider
-     * @param string $login
-     * @param string $resource
-     * @param int $status
-     */
-    public function testDelete($login, $resource, $status)
-    {
-        $this->loginUser($login);
-        $this->client->request(
-            'DELETE',
-            $this->getUrl(
-                'oro_api_customer_frontend_delete_customer_user',
-                ['id' => $this->getReference($resource)->getId()]
-            )
-        );
-        $this->assertResponseStatusCodeEquals($this->client->getResponse(), $status);
-
-        if ($status === 204) {
-            /** @var \Oro\Bundle\CustomerBundle\Entity\CustomerUser $user */
-            $user = $this->getUserRepository()->findOneBy(['email' => $resource]);
-            $this->assertNull($user);
-        }
-    }
-
-    /**
      * @return array
      */
     public function deleteDataProvider()
