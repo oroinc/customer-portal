f3dd443c640f4acbfe903779e55300d8334c074b
Yevhen Petrov <ypetrov@magecore.com>
2017-02-28 19:28:14 +0200

    BB-1210: JS routing on frontend - added config for  routes_to_expose
    (cherry picked from commit 4d57eab)

--- src/Oro/Bundle/FrontendBundle/DependencyInjection/Configuration.php
+++ src/Oro/Bundle/FrontendBundle/DependencyInjection/Configuration.php
@@ -24,6 +24,14 @@ class Configuration implements ConfigurationInterface
                 'page_templates' => ['type' => 'array', 'value' => []],
             ]
         );
+        $rootNode->children()
+            ->arrayNode('routes_to_expose')
+                ->beforeNormalization()
+                    ->ifTrue(function ($v) { return !is_array($v); })
+                    ->then(function ($v) { return array($v); })
+                ->end()
+                ->prototype('scalar')
+            ->end();
 
         return $treeBuilder;
     }
