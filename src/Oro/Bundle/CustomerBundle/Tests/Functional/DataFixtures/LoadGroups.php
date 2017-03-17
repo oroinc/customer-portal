5dde13a89bf8f2956eaa933f7aa24ab0d036187e
Serj Bielanovskyi <sprightly@users.noreply.github.com>
2017-03-17 13:36:12 +0200

    BB-7637: Category URLs provider (#8293)
    * BB-7637: Category URLs provider
    - added listener that restrict categories for sitemap by visibility
    - covered UrlItemsProvider feature state related case by test
    - add canonical security type option on website level

--- src/Oro/Bundle/CustomerBundle/Tests/Functional/DataFixtures/LoadGroups.php
+++ src/Oro/Bundle/CustomerBundle/Tests/Functional/DataFixtures/LoadGroups.php
@@ -6,16 +6,33 @@ use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
 use Oro\Bundle\CustomerBundle\Entity\CustomerGroup;
+use Oro\Bundle\CustomerBundle\Migrations\Data\ORM\LoadAnonymousCustomerGroup;
 use Oro\Bundle\UserBundle\DataFixtures\UserUtilityTrait;
 use Oro\Bundle\UserBundle\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class LoadGroups extends AbstractFixture
+class LoadGroups extends AbstractFixture implements ContainerAwareInterface
 {
     use UserUtilityTrait;
+
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
 
     const GROUP1 = 'customer_group.group1';
     const GROUP2 = 'customer_group.group2';
     const GROUP3 = 'customer_group.group3';
+    const ANONYMOUS_GROUP = 'customer_group.anonymous';
 
     /**
      * {@inheritdoc}
@@ -26,6 +43,7 @@ class LoadGroups extends AbstractFixture
         $this->createGroup($manager, self::GROUP1, $owner);
         $this->createGroup($manager, self::GROUP2, $owner);
         $this->createGroup($manager, self::GROUP3, $owner);
+        $this->loadAnonymousGroup();
 
         $manager->flush();
     }
@@ -46,5 +64,15 @@ class LoadGroups extends AbstractFixture
         $this->addReference($name, $group);
 
         return $group;
+    }
+
+    protected function loadAnonymousGroup()
+    {
+        $group = $this->container
+            ->get('doctrine')
+            ->getManagerForClass('OroCustomerBundle:CustomerGroup')
+            ->getRepository('OroCustomerBundle:CustomerGroup')
+            ->findOneBy(['name' => LoadAnonymousCustomerGroup::GROUP_NAME_NON_AUTHENTICATED]);
+        $this->addReference(self::ANONYMOUS_GROUP, $group);
     }
 }
