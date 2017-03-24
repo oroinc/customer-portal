8eb8cfc84fb31e0bce87711743e942abe5362df5
Jakub Derdziak <jderdziak@oroinc.com>
2017-03-24 07:49:52 +0100

    BB-8444: Prepare template fixture - created fixture to generate customer user - updated config of import/export

---
+++ src/Oro/Bundle/CustomerBundle/ImportExport/TemplateFixture/CustomerUserFixture.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Oro\Bundle\CustomerBundle\ImportExport\TemplateFixture;
+
+use Oro\Bundle\CustomerBundle\Entity\Customer;
+use Oro\Bundle\CustomerBundle\Entity\CustomerUser;
+use Oro\Bundle\ImportExportBundle\TemplateFixture\AbstractTemplateRepository;
+use Oro\Bundle\ImportExportBundle\TemplateFixture\TemplateFixtureInterface;
+
+class CustomerUserFixture extends AbstractTemplateRepository implements TemplateFixtureInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getEntityClass()
+    {
+        return CustomerUser::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function fillEntityData($key, $entity)
+    {
+        switch ($key) {
+            case 'Jerry Coleman':
+                $entity->setCustomer((new Customer())->setName('Oro Inc.'))
+                    ->setFirstName('Jerry')
+                    ->setLastName('Coleman')
+                    ->setEmail('example@email.com')
+                    ->setConfirmed(false)
+                    ->setEnabled(true);
+
+                return;
+        }
+
+        parent::fillEntityData($key, $entity);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getData()
+    {
+        return $this->getEntityData('Jerry Coleman');
+    }
+
+    /**
+     * Creates a new instance of the entity
+     *
+     * @param string $key
+     *
+     * @return object
+     */
+    protected function createEntity($key)
+    {
+        return new CustomerUser();
+    }
+}
