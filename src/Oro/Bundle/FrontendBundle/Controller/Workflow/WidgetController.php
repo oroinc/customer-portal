e2deed5fbba79a5aa8495e17d049d711ed6da5ce
Nikita Makarov <nmakarov@oroinc.com>
2017-03-31 19:00:25 +0300

    BAP-14400: Create controller actions with layout renderer for start transition form - Layout configuration updated - Removed unused StartTransitionDataProvider - Removed unused configuration files

--- src/Oro/Bundle/FrontendBundle/Controller/Workflow/WidgetController.php
+++ src/Oro/Bundle/FrontendBundle/Controller/Workflow/WidgetController.php
@@ -2,24 +2,25 @@
 
 namespace Oro\Bundle\FrontendBundle\Controller\Workflow;
 
-use Oro\Bundle\WorkflowBundle\Exception\ForbiddenTransitionException;
-use Oro\Bundle\WorkflowBundle\Exception\InvalidTransitionException;
-use Oro\Bundle\WorkflowBundle\Exception\WorkflowNotFoundException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 use Oro\Bundle\LayoutBundle\Annotation\Layout;
+use Oro\Bundle\WorkflowBundle\Configuration\FeatureConfigurationExtension;
 use Oro\Bundle\WorkflowBundle\Entity\WorkflowItem;
+use Oro\Bundle\WorkflowBundle\Exception\ForbiddenTransitionException;
+use Oro\Bundle\WorkflowBundle\Exception\InvalidTransitionException;
+use Oro\Bundle\WorkflowBundle\Exception\WorkflowNotFoundException;
 use Oro\Bundle\WorkflowBundle\Form\Handler\TransitionFormHandlerInterface;
 use Oro\Bundle\WorkflowBundle\Helper\TransitionWidgetHelper;
 use Oro\Bundle\WorkflowBundle\Model\Transition;
+use Oro\Bundle\WorkflowBundle\Model\WorkflowData;
 use Oro\Bundle\WorkflowBundle\Model\WorkflowManager;
 
 class WidgetController extends Controller
@@ -36,10 +37,93 @@ class WidgetController extends Controller
      * @param string $workflowName
      * @param Request $request
      *
-     * @return Response
+     * @return Response|array
      */
     public function startTransitionFormAction($transitionName, $workflowName, Request $request)
     {
+        $entityId = $request->get('entityId', 0);
+        /** @var WorkflowManager $workflowManager */
+        $workflowManager = $this->get('oro_workflow.manager');
+        $workflow = $workflowManager->getWorkflow($workflowName);
+        $entityClass = $workflow->getDefinition()->getRelatedEntity();
+        $transition = $workflow->getTransitionManager()->extractTransition($transitionName);
+        $dataArray = [];
+
+        if (!$transition->isEmptyInitOptions()) {
+            $contextAttribute = $transition->getInitContextAttribute();
+            $dataArray[$contextAttribute] = $this->get('oro_action.provider.button_search_context')
+                ->getButtonSearchContext();
+            $entityId = null;
+        }
+
+        $entity = $this->getTransitionWidgetHelper()->getOrCreateEntityReference($entityClass, $entityId);
+        $workflowItem = $workflow->createWorkflowItem($entity, $dataArray);
+
+        $transitionForm = $this->get('oro_workflow.layout.data_provider.transition_form')
+            ->getTransitionForm($transitionName, $workflowItem);
+
+        if ($request->isMethod('POST')) {
+            $saved = $this->getTransitionFormHandler($transition)
+                ->processStartTransitionForm($transitionForm, $workflowItem, $transition, $request);
+            if ($saved) {
+                $data = $this->getTransitionWidgetHelper()
+                    ->processWorkflowData($workflow, $transition, $transitionForm, $dataArray);
+
+                try {
+                    if (!$this->get('oro_featuretoggle.checker.feature_checker')
+                        ->isResourceEnabled($workflowName, FeatureConfigurationExtension::WORKFLOWS_NODE_NAME)
+                    ) {
+                        throw new ForbiddenTransitionException();
+                    }
+                    $dataArray = [];
+                    if ($data) {
+                        $serializer = $this->get('oro_workflow.serializer.data.serializer');
+                        $serializer->setWorkflowName($workflowName);
+                        /* @var $data WorkflowData */
+                        $data = $serializer->deserialize(
+                            $data,
+                            'Oro\Bundle\WorkflowBundle\Model\WorkflowData',
+                            'json'
+                        );
+                        $dataArray = $data->getValues();
+                    }
+
+                    $workflowItem = $workflowManager->startWorkflow($workflowName, $entity, $transition, $dataArray);
+                } catch (WorkflowNotFoundException $e) {
+                } catch (InvalidTransitionException $e) {
+                } catch (ForbiddenTransitionException $e) {
+                } catch (\Exception $e) {
+                }
+
+                if (!isset($e)) {
+                    $url = '/';
+                    if ($workflowItem->getResult()->get('redirectUrl')) {
+                        $url = $workflowItem->getResult()->get('redirectUrl');
+                    } else {
+                        if ($request->headers->get('referer')) {
+                            $url = $request->headers->get('referer');
+                        }
+                    }
+                    if ($request->isXmlHttpRequest()) {
+                        return JsonResponse::create([
+                            'workflowItem' => ['result' => ['redirectUrl' => $url]]
+                        ]);
+                    } else {
+                        return $this->redirect($url);
+                    }
+                }
+            }
+        }
+
+        return [
+            'data' => [
+                'workflowName' => $workflowName,
+                'workflowItem' => $workflowItem,
+                'transitionName' => $transitionName,
+                'entityId' => $request->get('entityId', 0),
+                'formRouteName' => 'oro_frontend_workflow_widget_start_transition_form',
+            ]
+        ];
     }
 
     /**
--- src/Oro/Bundle/FrontendBundle/Controller/Workflow/WorkflowController.php
+++ src/Oro/Bundle/FrontendBundle/Controller/Workflow/WorkflowController.php
@@ -6,18 +6,21 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 use Oro\Bundle\LayoutBundle\Annotation\Layout;
+use Oro\Bundle\WorkflowBundle\Configuration\FeatureConfigurationExtension;
 use Oro\Bundle\WorkflowBundle\Entity\WorkflowItem;
 use Oro\Bundle\WorkflowBundle\Exception\ForbiddenTransitionException;
 use Oro\Bundle\WorkflowBundle\Exception\InvalidTransitionException;
 use Oro\Bundle\WorkflowBundle\Exception\WorkflowNotFoundException;
-use Oro\Bundle\WorkflowBundle\Helper\TransitionWidgetHelper;
 use Oro\Bundle\WorkflowBundle\Form\Handler\TransitionFormHandlerInterface;
+use Oro\Bundle\WorkflowBundle\Helper\TransitionWidgetHelper;
 use Oro\Bundle\WorkflowBundle\Model\Transition;
+use Oro\Bundle\WorkflowBundle\Model\WorkflowData;
 use Oro\Bundle\WorkflowBundle\Model\WorkflowManager;
 
 class WorkflowController extends Controller
@@ -28,7 +31,7 @@ class WorkflowController extends Controller
      *      name="oro_frontend_workflow_start_transition_form"
      * )
      *
-     * @Layout
+     * @Layout(vars={"transitionName", "workflowName"})
      *
      * @param string $workflowName
      * @param string $transitionName
@@ -38,10 +41,84 @@ class WorkflowController extends Controller
      */
     public function startTransitionAction($workflowName, $transitionName, Request $request)
     {
+        $entityId = $request->get('entityId', 0);
+        /** @var WorkflowManager $workflowManager */
+        $workflowManager = $this->get('oro_workflow.manager');
+        $workflow = $workflowManager->getWorkflow($workflowName);
+        $entityClass = $workflow->getDefinition()->getRelatedEntity();
+        $transition = $workflow->getTransitionManager()->extractTransition($transitionName);
+        $dataArray = [];
+
+        if (!$transition->isEmptyInitOptions()) {
+            $contextAttribute = $transition->getInitContextAttribute();
+            $dataArray[$contextAttribute] = $this->get('oro_action.provider.button_search_context')
+                ->getButtonSearchContext();
+            $entityId = null;
+        }
+
+        $entity = $this->getTransitionWidgetHelper()->getOrCreateEntityReference($entityClass, $entityId);
+        $workflowItem = $workflow->createWorkflowItem($entity, $dataArray);
+
+        $transitionForm = $this->get('oro_workflow.layout.data_provider.transition_form')
+            ->getTransitionForm($transitionName, $workflowItem);
+
+        if ($request->isMethod('POST')) {
+            $saved = $this->getTransitionFormHandler($transition)
+                ->processStartTransitionForm($transitionForm, $workflowItem, $transition, $request);
+            if ($saved) {
+                $data = $this->getTransitionWidgetHelper()
+                    ->processWorkflowData($workflow, $transition, $transitionForm, $dataArray);
+
+                try {
+                    if (!$this->get('oro_featuretoggle.checker.feature_checker')
+                        ->isResourceEnabled($workflowName, FeatureConfigurationExtension::WORKFLOWS_NODE_NAME)
+                    ) {
+                        throw new ForbiddenTransitionException();
+                    }
+                    $dataArray = [];
+                    if ($data) {
+                        $serializer = $this->get('oro_workflow.serializer.data.serializer');
+                        $serializer->setWorkflowName($workflowName);
+                        /* @var $data WorkflowData */
+                        $data = $serializer->deserialize(
+                            $data,
+                            'Oro\Bundle\WorkflowBundle\Model\WorkflowData',
+                            'json'
+                        );
+                        $dataArray = $data->getValues();
+                    }
+
+                    $workflowItem = $workflowManager->startWorkflow($workflowName, $entity, $transition, $dataArray);
+                } catch (WorkflowNotFoundException $e) {
+                } catch (InvalidTransitionException $e) {
+                } catch (ForbiddenTransitionException $e) {
+                } catch (\Exception $e) {
+                }
+
+                if (!isset($e)) {
+                    $url = '/';
+                    if ($workflowItem->getResult()->get('redirectUrl')) {
+                        $url = $workflowItem->getResult()->get('redirectUrl');
+                    } elseif ($request->headers->get('referer')) {
+                        $url = $request->headers->get('referer');
+                    } elseif ($request->get('originalUrl', '')) {
+                        $url = $request->get('originalUrl', '/');
+                    }
+
+                    return $this->redirect($url);
+                }
+            }
+        }
+
         return [
+            'workflowName' => $workflowItem->getWorkflowName(),
+            'transitionName' => $transition->getName(),
             'data' => [
                 'workflowName' => $workflowName,
+                'workflowItem' => $workflowItem,
                 'transitionName' => $transitionName,
+                'entityId' => $request->get('entityId', 0),
+                'originalUrl' => $request->get('originalUrl', '/'),
                 'formRouteName' => 'oro_frontend_workflow_start_transition_form',
             ]
         ];
@@ -89,10 +166,10 @@ class WorkflowController extends Controller
                     $url = '/';
                     if ($workflowItem->getResult()->get('redirectUrl')) {
                         $url = $workflowItem->getResult()->get('redirectUrl');
-                    } else {
-                        if ($request->headers->get('referer')) {
-                            $url = $request->headers->get('referer');
-                        }
+                    } elseif ($request->headers->get('referer')) {
+                        $url = $request->headers->get('referer');
+                    } elseif ($request->get('originalUrl', '')) {
+                        $url = $request->get('originalUrl', '/');
                     }
 
                     return $this->redirect($url);
@@ -107,6 +184,7 @@ class WorkflowController extends Controller
                 'transition' => $transition,
                 'workflowItem' => $workflowItem,
                 'formRouteName' => 'oro_frontend_workflow_transition_form',
+                'originalUrl' => $request->get('originalUrl', '/'),
             ]
         ];
     }
