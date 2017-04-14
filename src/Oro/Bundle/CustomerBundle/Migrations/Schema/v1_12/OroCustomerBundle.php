e08279dc64e48aa0d3f6ee0d93645a79b7f29950
DenysZakharov <dzakharov@oroinc.com>
2017-04-14 11:28:52 +0300

    BB-7171: Frontend Grid Views - add fix test

--- src/Oro/Bundle/CustomerBundle/Migrations/Schema/v1_12/OroCustomerBundle.php
+++ src/Oro/Bundle/CustomerBundle/Migrations/Schema/v1_12/OroCustomerBundle.php
@@ -24,14 +24,6 @@ class OroCustomerBundle implements Migration
         $this->removeFromConfig($queries, 'default_customer_owner');
 
         $this->addOwnership($schema);
-
-        /** Tables modifications **/
-        $this->updateOroGridViewTable($schema);
-        $this->updateOroGridViewUserTable($schema);
-
-        /** Foreign keys generation **/
-        $this->addOroGridViewForeignKeys($schema);
-        $this->addOroGridViewUserForeignKeys($schema);
     }
 
     /**
@@ -63,62 +55,6 @@ class OroCustomerBundle implements Migration
         $table->addForeignKeyConstraint(
             $schema->getTable('oro_organization'),
             ['organization_id'],
-            ['id'],
-            ['onDelete' => 'SET NULL', 'onUpdate' => null]
-        );
-    }
-
-    /**
-     * Update oro_grid_view table
-     *
-     * @param Schema $schema
-     */
-    private function updateOroGridViewTable(Schema $schema)
-    {
-        $table = $schema->getTable('oro_grid_view');
-        $table->addColumn('customer_user_owner_id', 'integer', ['notnull' => false]);
-        $table->addIndex(['customer_user_owner_id'], 'idx_oro_grid_view_cust_user');
-    }
-
-    /**
-     * Update oro_grid_view_user_rel table
-     *
-     * @param Schema $schema
-     */
-    private function updateOroGridViewUserTable(Schema $schema)
-    {
-        $table = $schema->getTable('oro_grid_view_user_rel');
-        $table->addColumn('customer_user_id', 'integer', ['notnull' => false]);
-        $table->addIndex(['customer_user_id'], 'idx_oro_grid_view_user_cust_user');
-    }
-
-    /**
-     * Add oro_grid_view foreign keys
-     *
-     * @param Schema $schema
-     */
-    private function addOroGridViewForeignKeys(Schema $schema)
-    {
-        $table = $schema->getTable('oro_grid_view');
-        $table->addForeignKeyConstraint(
-            $schema->getTable('oro_customer_user'),
-            ['customer_user_owner_id'],
-            ['id'],
-            ['onDelete' => 'CASCADE', 'onUpdate' => null]
-        );
-    }
-
-    /**
-     * Add oro_grid_view_user_rel foreign keys
-     *
-     * @param Schema $schema
-     */
-    private function addOroGridViewUserForeignKeys(Schema $schema)
-    {
-        $table = $schema->getTable('oro_grid_view_user_rel');
-        $table->addForeignKeyConstraint(
-            $schema->getTable('oro_customer_user'),
-            ['customer_user_id'],
             ['id'],
             ['onDelete' => 'SET NULL', 'onUpdate' => null]
         );
