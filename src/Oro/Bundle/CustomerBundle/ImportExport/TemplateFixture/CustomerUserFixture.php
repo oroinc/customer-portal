4ddb87b6b6af48d8368120ce71e7eec8aca13793
Mateusz Fejczaruk <mfejczaruk@gmail.com>
2017-04-20 13:30:51 +0200

    BB-8802: Adjust import data template (#9728)
    - Add user template fixture

--- src/Oro/Bundle/CustomerBundle/ImportExport/TemplateFixture/CustomerUserFixture.php
+++ src/Oro/Bundle/CustomerBundle/ImportExport/TemplateFixture/CustomerUserFixture.php
@@ -2,10 +2,15 @@
 
 namespace Oro\Bundle\CustomerBundle\ImportExport\TemplateFixture;
 
+use Doctrine\Common\Collections\ArrayCollection;
+
 use Oro\Bundle\CustomerBundle\Entity\Customer;
 use Oro\Bundle\CustomerBundle\Entity\CustomerUser;
+use Oro\Bundle\CustomerBundle\Entity\CustomerUserRole;
 use Oro\Bundle\ImportExportBundle\TemplateFixture\AbstractTemplateRepository;
 use Oro\Bundle\ImportExportBundle\TemplateFixture\TemplateFixtureInterface;
+use Oro\Bundle\UserBundle\Entity\User;
+use Oro\Bundle\WebsiteBundle\Entity\Website;
 
 class CustomerUserFixture extends AbstractTemplateRepository implements TemplateFixtureInterface
 {
@@ -24,12 +29,26 @@ class CustomerUserFixture extends AbstractTemplateRepository implements Template
     {
         switch ($key) {
             case 'Jerry Coleman':
+                $owner = (new User())->setId(1);
+                $dateTime = (new \DateTime())->setTimestamp(545843181);
+                $customerRole = $this->generateCustomerRole();
+                $website = $this->generateWebsite();
+
                 $entity->setCustomer((new Customer())->setName('Oro Inc.'))
                     ->setFirstName('Jerry')
                     ->setLastName('Coleman')
                     ->setEmail('example@email.com')
                     ->setConfirmed(false)
-                    ->setEnabled(true);
+                    ->setEnabled(true)
+                    ->setOwner($owner)
+                    ->setWebsite($website)
+                    ->setBirthday($dateTime)
+                    ->setNamePrefix('Mr')
+                    ->setNameSuffix('Jr.')
+                    ->setNameSuffix('Jr.')
+                    ->setMiddleName('John');
+
+                $this->addRole($entity, $customerRole);
 
                 return;
         }
@@ -51,5 +70,48 @@ class CustomerUserFixture extends AbstractTemplateRepository implements Template
     protected function createEntity($key)
     {
         return new CustomerUser();
+    }
+
+    /**
+     * @return Website
+     */
+    private function generateWebsite()
+    {
+        $website = new Website();
+        $reflectionObject = new \ReflectionObject($website);
+        $property = $reflectionObject->getProperty('id');
+        $property->setAccessible(true);
+        $property->setValue($website, 1);
+        $property->setAccessible(false);
+
+        return $website;
+    }
+
+    /**
+     * @return CustomerUserRole
+     */
+    private function generateCustomerRole()
+    {
+        $customerRole = new CustomerUserRole();
+        $reflectionObject = new \ReflectionObject($customerRole);
+        $property = $reflectionObject->getProperty('id');
+        $property->setAccessible(true);
+        $property->setValue($customerRole, 1);
+        $property->setAccessible(false);
+
+        return $customerRole;
+    }
+
+    /**
+     * @param $entity
+     * @param $customerRole
+     */
+    private function addRole($entity, $customerRole)
+    {
+        $reflectionObject = new \ReflectionObject($entity);
+        $property = $reflectionObject->getProperty('roles');
+        $property->setAccessible(true);
+        $property->setValue($entity, new ArrayCollection([$customerRole]));
+        $property->setAccessible(false);
     }
 }
