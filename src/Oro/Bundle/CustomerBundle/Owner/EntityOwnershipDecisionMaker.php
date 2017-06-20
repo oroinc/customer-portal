02574e0b6abab9bca78b8b3ed62c6202508718e1
Viacheslav Bemza <vbemza@magecore.com>
2017-06-20 16:16:48 +0300

    BB-9352: FrontStore > Account > Quotes > Amanda has not permission to see quote without Owner (#11030)
    fixed issue
    covered with behat tests

--- src/Oro/Bundle/CustomerBundle/Owner/EntityOwnershipDecisionMaker.php
+++ src/Oro/Bundle/CustomerBundle/Owner/EntityOwnershipDecisionMaker.php
@@ -2,16 +2,58 @@
 
 namespace Oro\Bundle\CustomerBundle\Owner;
 
+use Oro\Bundle\CustomerBundle\Entity\Repository\CustomerRepository;
 use Oro\Bundle\SecurityBundle\Owner\AbstractEntityOwnershipDecisionMaker;
 use Oro\Bundle\CustomerBundle\Entity\CustomerUser;
 
 class EntityOwnershipDecisionMaker extends AbstractEntityOwnershipDecisionMaker
 {
     /**
+     * @var CustomerRepository
+     */
+    private $customerRepository;
+
+    /**
      * {@inheritdoc}
      */
     public function supports()
     {
         return $this->getContainer()->get('oro_security.security_facade')->getLoggedUser() instanceof CustomerUser;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    // TODO: please remove this workaround after BB-10196
+    public function isAssociatedWithLocalLevelEntity($user, $domainObject, $deep = false, $organization = null)
+    {
+        $isAssociated = parent::isAssociatedWithLocalLevelEntity($user, $domainObject, $deep, $organization);
+
+        if (!$isAssociated && $deep) {
+            $metadata = $this->getObjectMetadata($domainObject);
+            if ($metadata->isBasicLevelOwned() && method_exists($domainObject, 'getCustomer')) {
+                $customerId = $this->getObjectId($user->getCustomer());
+                $ownerId = $this->getObjectIdIgnoreNull($domainObject->getCustomer());
+                $isAssociated = $customerId === $ownerId;
+                if (!$isAssociated) {
+                    $childrenIds = $this->getCustomerRepository()->getChildrenIds($customerId);
+                    $isAssociated = in_array($ownerId, $childrenIds, true);
+                }
+            }
+        }
+
+        return $isAssociated;
+    }
+
+    /**
+     * @return CustomerRepository
+     */
+    private function getCustomerRepository()
+    {
+        if (!$this->customerRepository) {
+            $this->customerRepository = $this->getContainer()->get('oro_customer.repository.customer');
+        }
+
+        return $this->customerRepository;
     }
 }
