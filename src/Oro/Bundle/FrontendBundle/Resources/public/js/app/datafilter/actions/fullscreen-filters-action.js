2aac25035d38f8d579bf283e1335205db028d36f
Valerii Yustyniuk <vyustyniuk@oroinc.com>
2017-06-23 17:04:27 +0300

    BB-9580: Mobile Filters - add fullscreen action for filters in frontend

---
+++ src/Oro/Bundle/FrontendBundle/Resources/public/js/app/datafilter/actions/fullscreen-filters-action.js
@@ -0,0 +1,75 @@
+define(function(require) {
+    'use strict';
+
+    var FrontendFullScreenFiltersAction;
+    var _ = require('underscore');
+    var mediator = require('oroui/js/mediator');
+    var FiltersManager = require('orofilter/js/filters-manager');
+    var ToggleFiltersAction = require('orofilter/js/actions/toggle-filters-action');
+    var FullscreenPopupView = require('orofrontend/blank/js/app/views/fullscreen-popup-view');
+    var module = require('module');
+    var config = module.config();
+
+    config = _.extend({
+        popupOptions: {}
+    }, config);
+
+    FrontendFullScreenFiltersAction =  ToggleFiltersAction.extend({
+        popupOptions: {
+            popupBadge: true,
+            popupIcon: 'fa-filter',
+            popupLabel: _.__('oro.filter.datagrid-toolbar.filters'),
+            contentElement: null
+        },
+
+        lock: false,
+
+        initialize: function(options) {
+            this.popupOptions = _.extend(this.popupOptions, options.popupOptions || {}, config.popupOptions);
+
+            FrontendFullScreenFiltersAction.__super__.initialize.apply(this, arguments);
+        },
+
+        execute: function() {
+            this.$filters = this.datagrid.filterManager.$el;
+            this.popupOptions.contentElement = this.$filters;
+
+            if (!this.lock) {
+                this.prepareContent(this.$filters);
+
+                this.lock = true;
+            }
+
+            this.fullscreenView = new FullscreenPopupView(this.popupOptions);
+
+            this.fullscreenView.on('show', function() {
+                // todo: Show filters <-- change comment
+                this.$filters.show();
+            }, this);
+            this.fullscreenView.on('close', function() {
+                // todo: Hide filters <-- change comment
+                this.$filters.hide();
+
+                this.fullscreenView.off();
+                this.fullscreenView.dispose();
+                delete this.fullscreenView;
+            }, this);
+
+            this.fullscreenView.show();
+        },
+
+        onFilterManagerModeChange: function(mode) {
+            if (this.launcherInstanse) {
+                this.launcherInstanse.$el.toggleClass('pressed', mode === FiltersManager.MANAGE_VIEW_MODE);
+            }
+            mediator.trigger('layout:adjustHeight');
+        },
+
+        prepareContent: function($container) {
+            $container.find('.filter-criteria-selector').removeClass('btn oro-drop-opener oro-dropdown-toggle');
+            $container.find('.filter-criteria').removeClass('dropdown-menu');
+        }
+    });
+
+    return FrontendFullScreenFiltersAction;
+});
---
+++ src/Oro/Bundle/FrontendBundle/Resources/public/js/app/datafilter/plugins/frontend-filters-plugin.js
@@ -0,0 +1,37 @@
+define(function(require) {
+    'use strict';
+
+    var FrontendFiltersTogglePlugin;
+    var _ = require('underscore');
+    var __ = require('orotranslation/js/translator');
+
+    var ToggleFiltersAction = require('orofilter/js/actions/toggle-filters-action');
+    var FullScreenFiltersAction = require('orofrontend/js/app/datafilter/actions/fullscreen-filters-action');
+    var FiltersTogglePlugin = require('orofilter/js/plugins/filters-toggle-plugin');
+    var config = require('module').config();
+    var launcherOptions = _.extend({
+        className: 'btn',
+        icon: 'filter',
+        label: __('oro.filter.datagrid-toolbar.filters')
+    }, config.launcherOptions || {});
+
+    FrontendFiltersTogglePlugin = FiltersTogglePlugin.extend({
+        useFullScreenMode: _.isMobile(),
+
+        onBeforeToolbarInit: function(toolbarOptions) {
+            var options = {
+                datagrid: this.main,
+                launcherOptions: launcherOptions,
+                order: config.order || 50
+            };
+
+            if (this.useFullScreenMode) {
+                toolbarOptions.addToolbarAction(new FullScreenFiltersAction(options));
+            } else {
+                toolbarOptions.addToolbarAction(new ToggleFiltersAction(options));
+            }
+        }
+    });
+
+    return FrontendFiltersTogglePlugin;
+});
