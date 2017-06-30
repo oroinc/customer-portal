3daaaff60374b2e6a0c06e89657588ae4cee8880
uladzimirkuzmin <vkuzmin@oroinc.com>
2017-06-30 12:26:19 +0300

    BB-10496: Allow to configure fullscreen popup in column-manager-component - added white space

--- src/Oro/Bundle/FrontendBundle/Resources/public/js/app/views/column-manager/frontend-column-manager-view.js
+++ src/Oro/Bundle/FrontendBundle/Resources/public/js/app/views/column-manager/frontend-column-manager-view.js
@@ -11,7 +11,7 @@ define(function(require) {
 
     config = _.extend({
         className: 'dropdown-menu',
-        viewport:{
+        viewport: {
             maxScreenType: 'mobile-landscape'
         },
         popupOptions: {},
