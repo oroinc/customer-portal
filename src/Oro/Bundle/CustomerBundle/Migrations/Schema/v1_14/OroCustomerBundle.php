0dfbe226780fca20df4e040524e3237c08ef9c83
Vladimir Seniuk <vseniuk@oroinc.com>
2017-07-05 15:39:20 +0300

    BB-9992: Required Update entities after fresh installation - added required changes in admin templates for supporting extended fields - fixed migration

--- src/Oro/Bundle/CustomerBundle/Migrations/Schema/v1_14/OroCustomerBundle.php
+++ src/Oro/Bundle/CustomerBundle/Migrations/Schema/v1_14/OroCustomerBundle.php
@@ -7,32 +7,15 @@ use Doctrine\DBAL\Schema\Schema;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
-use Oro\Bundle\CustomerBundle\Entity\CustomerUser;
-use Oro\Bundle\CustomerBundle\Entity\CustomerUserRole;
-use Oro\Bundle\EntityConfigBundle\Config\ConfigInterface;
-use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
-use Oro\Bundle\MigrationBundle\Migration\Extension\DataStorageExtension;
-use Oro\Bundle\MigrationBundle\Migration\Extension\DataStorageExtensionAwareInterface;
+use Oro\Bundle\EntityConfigBundle\Migration\UpdateEntityExtendConfigMigrationQuery;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
 class OroCustomerBundle implements
     Migration,
-    ContainerAwareInterface,
-    DataStorageExtensionAwareInterface
+    ContainerAwareInterface
 {
     use ContainerAwareTrait;
-
-    /** @var DataStorageExtension */
-    private $dataStorageExtension;
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setDataStorageExtension(DataStorageExtension $dataStorageExtension)
-    {
-        $this->dataStorageExtension = $dataStorageExtension;
-    }
 
     /**
      * {@inheritdoc}
@@ -41,7 +24,11 @@ class OroCustomerBundle implements
     {
         /** Tables generation **/
         $this->createCustomerVisitorTable($schema);
-        $this->updateConfig();
+
+        $commandExecutor = $this->container->get('oro_entity_config.tools.command_executor');
+        $queries->addPostQuery(
+            new UpdateEntityExtendConfigMigrationQuery($commandExecutor)
+        );
     }
 
     /**
@@ -57,39 +44,5 @@ class OroCustomerBundle implements
         $table->addColumn('session_id', 'string', ['length' => 255]);
         $table->setPrimaryKey(['id']);
         $table->addIndex(['id', 'session_id'], 'id_session_id_idx');
-    }
-
-    private function updateConfig()
-    {
-        $storage = $this->dataStorageExtension;
-        $stateData = [];
-
-        $dumper = $this->container->get('oro_entity_extend.tools.dumper');
-        $dumper->updateConfig(function (ConfigInterface $config) use ($storage, $stateData) {
-            $configId  = $config->getId();
-            $className = $configId->getClassName();
-
-            $isSupported = $className === CustomerUser::class || $className === CustomerUserRole::class;
-
-            if ($isSupported) {
-                $stateData = $storage->get('initial_entity_config_state', []);
-                $stateData['entities'][$className] = $config->get('state');
-            }
-
-            return $isSupported;
-        });
-
-        $this->dataStorageExtension->set('initial_entity_config_state', $stateData);
-
-        $configManager = $this->container->get('oro_entity_config.config_manager');
-        $provider = $configManager->getProvider('extend');
-
-        $entityConfig = $provider->getConfig(CustomerUser::class);
-        $entityConfig->set('state', ExtendScope::STATE_ACTIVE);
-        $configManager->persist($entityConfig);
-
-        $entityConfig = $provider->getConfig(CustomerUserRole::class);
-        $entityConfig->set('state', ExtendScope::STATE_ACTIVE);
-        $configManager->persist($entityConfig);
     }
 }
