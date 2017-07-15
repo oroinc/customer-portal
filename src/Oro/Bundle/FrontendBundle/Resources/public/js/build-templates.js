6e4550f2bfe84c7f86f378350ca2fb2d60565a54
Oleksandr Nechyporenko <4alexandr@users.noreply.github.com>
2017-07-15 11:41:12 +0300

    BB-10840: Frontstore>Empty grids :in console Module name "tpl!orofrontend/default/templates/ui/modal-dialog.html" has not been loaded yet for context: _ (#11809)
    - hotfix

--- src/Oro/Bundle/FrontendBundle/Resources/public/js/build-templates.js
+++ src/Oro/Bundle/FrontendBundle/Resources/public/js/build-templates.js
@@ -17,4 +17,22 @@ define(function(require) {
     require('text!oropricing/templates/order/frontend/subtotals.html');
     require('text!oropricing/templates/order/frontend/totals.html');
     require('text!orofrontend/default/templates/filter/filters-container.html');
+
+    //@todo: remove hotfix
+    require('tpl!orofrontend/default/templates/ui/modal-dialog.html');
+    require('tpl!orofrontend/default/templates/ui/message-item.html');
+    require('tpl!orofrontend/default/templates/datagrid/column-manager-collection.html');
+    require('tpl!orofrontend/default/templates/datagrid/column-manager-item.html');
+    require('tpl!orofrontend/default/templates/datagrid/column-manager-filter.html');
+    require('tpl!orofrontend/default/templates/datagrid/column-manager.html');
+    require('tpl!orofrontend/default/templates/datagrid/pagination-input.html');
+    require('tpl!orofrontend/default/templates/datagrid/page-size.html');
+    require('tpl!orofrontend/default/templates/datagrid/visible-items-counter.html');
+    require('tpl!orofrontend/default/templates/datagrid/select-all-header-cell.html');
+    require('tpl!orofrontend/default/templates/datagrid/select-row-cell.html');
+    require('tpl!orofrontend/default/templates/datagrid/action-header-cell.html');
+    require('tpl!orofrontend/default/templates/datagrid/grid-view-label.html');
+    require('tpl!oropricing/templates/order/frontend/subtotals.html');
+    require('tpl!oropricing/templates/order/frontend/totals.html');
+    require('tpl!orofrontend/default/templates/filter/filters-container.html');
 });
