a2012e20562316a16365df73eddadbb894043012
Sam Ivichuk <serious7sam@gmail.com>
2017-09-28 18:50:37 +0300

    BB-11448: Add new design for the import popup  - added unit tests

---
+++ src/Oro/Bundle/CustomerBundle/Tests/Unit/ImportExport/Configuration/CustomerImportExportConfigurationProviderTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Oro\Bundle\CustomerBundle\Tests\Unit\ImportExport\Configuration;
+
+use Oro\Bundle\CustomerBundle\Entity\Customer;
+use Oro\Bundle\CustomerBundle\ImportExport\Configuration\CustomerImportExportConfigurationProvider;
+use Oro\Bundle\ImportExportBundle\Configuration\ImportExportConfiguration;
+use PHPUnit\Framework\TestCase;
+
+class CustomerImportExportConfigurationProviderTest extends TestCase
+{
+    public function testGet()
+    {
+        static::assertEquals(
+            new ImportExportConfiguration([
+                ImportExportConfiguration::FIELD_ENTITY_CLASS => Customer::class,
+                ImportExportConfiguration::FIELD_EXPORT_TEMPLATE_PROCESSOR_ALIAS => 'oro_customer_customer',
+                ImportExportConfiguration::FIELD_EXPORT_PROCESSOR_ALIAS => 'oro_customer_customer',
+                ImportExportConfiguration::FIELD_IMPORT_PROCESSOR_ALIAS => 'oro_customer_customer',
+            ]),
+            (new CustomerImportExportConfigurationProvider())->get()
+        );
+    }
+}
