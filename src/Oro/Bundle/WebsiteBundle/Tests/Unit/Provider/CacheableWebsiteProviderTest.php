30d2095810502dd73ae2556ad0906d95e6737c88
Mykhailo Sulyma <msulima@oroinc.com>
2018-01-30 11:45:21 +0200

    BB-13491: SS_ Adding Warehouse: duplicated queries from oro_website (#16358)
    BB-13491: SS_ Adding Warehouse: duplicated queries from oro_website
     - added cache provider for websites
     - used CacheableWebsiteProvider in services instead WebsiteRepository
     - added custom doctrine event, which is fired when transaction is rolled back in test environment
     - fixed bug when CacheableWebsiteProvider isnt cleared between tests in test environment

---
+++ src/Oro/Bundle/WebsiteBundle/Tests/Unit/Provider/CacheableWebsiteProviderTest.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Oro\Bundle\WebsiteBundle\Tests\Unit\Provider;
+
+use Doctrine\Common\Cache\ArrayCache;
+use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
+use Oro\Bundle\WebsiteBundle\Entity\Website;
+use Oro\Bundle\WebsiteBundle\Provider\CacheableWebsiteProvider;
+use Oro\Bundle\WebsiteBundle\Provider\WebsiteProviderInterface;
+use Oro\Component\Testing\Unit\EntityTrait;
+
+class CacheableWebsiteProviderTest extends \PHPUnit_Framework_TestCase
+{
+    use EntityTrait;
+
+    /** @var WebsiteProviderInterface|\PHPUnit_Framework_MockObject_MockObject */
+    private $websiteProvider;
+
+    /** @var DoctrineHelper|\PHPUnit_Framework_MockObject_MockObject */
+    private $doctrineHelper;
+
+    /** @var CacheableWebsiteProvider */
+    private $cacheableProvider;
+
+    protected function setUp()
+    {
+        $this->websiteProvider = $this->createMock(WebsiteProviderInterface::class);
+        $this->doctrineHelper = $this->createMock(DoctrineHelper::class);
+
+        $this->cacheableProvider = new CacheableWebsiteProvider(
+            $this->websiteProvider,
+            new ArrayCache(),
+            $this->doctrineHelper
+        );
+    }
+
+    public function testGetWebsites()
+    {
+        $websiteId = 123;
+        $website = $this->getWebsite($websiteId);
+
+        $this->websiteProvider->expects($this->once())
+            ->method('getWebsiteIds')
+            ->willReturn([$websiteId]);
+
+        $this->doctrineHelper->expects($this->atLeastOnce())
+            ->method('getEntityReference')
+            ->with(Website::class, $websiteId)
+            ->willReturn($website);
+
+        $this->assertEquals([$website->getId() => $website], $this->cacheableProvider->getWebsites());
+        // test the result is cached
+        $this->assertEquals([$website->getId() => $website], $this->cacheableProvider->getWebsites());
+    }
+
+    public function testGetWebsiteIds()
+    {
+        $ids = [1001, 1002, 1003];
+
+        $this->websiteProvider->expects($this->once())
+            ->method('getWebsiteIds')
+            ->willReturn($ids);
+
+        $this->assertEquals($ids, $this->cacheableProvider->getWebsiteIds());
+        // test the result is cached
+        $this->assertEquals($ids, $this->cacheableProvider->getWebsiteIds());
+    }
+
+    public function testHasCacheAndClearCacheForGetWebsites()
+    {
+        $websiteId = 123;
+        $website = $this->getWebsite($websiteId);
+
+        $this->websiteProvider->expects($this->once())
+            ->method('getWebsiteIds')
+            ->willReturn([$websiteId]);
+
+        $this->doctrineHelper->expects($this->atLeastOnce())
+            ->method('getEntityReference')
+            ->with(Website::class, $websiteId)
+            ->willReturn($website);
+
+        $this->assertFalse($this->cacheableProvider->hasCache());
+
+        $this->cacheableProvider->getWebsites();
+        $this->assertTrue($this->cacheableProvider->hasCache());
+
+        $this->cacheableProvider->clearCache();
+        $this->assertFalse($this->cacheableProvider->hasCache());
+    }
+
+    public function testHasCacheAndClearCacheForGetWebsiteIds()
+    {
+        $this->websiteProvider->expects($this->once())
+            ->method('getWebsiteIds')
+            ->willReturn([1001, 1002, 1003]);
+
+        $this->assertFalse($this->cacheableProvider->hasCache());
+
+        $this->cacheableProvider->getWebsiteIds();
+        $this->assertTrue($this->cacheableProvider->hasCache());
+
+        $this->cacheableProvider->clearCache();
+        $this->assertFalse($this->cacheableProvider->hasCache());
+    }
+
+    /**
+     * @param int $id
+     * @return object|Website
+     */
+    protected function getWebsite($id)
+    {
+        return $this->getEntity(Website::class, ['id' => $id]);
+    }
+}
