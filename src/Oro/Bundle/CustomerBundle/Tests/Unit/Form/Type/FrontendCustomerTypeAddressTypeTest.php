33f813f2aff00af50d723986175c5d8b909f169f
Vitaliy Berdylo <vberdylo@oroinc.com>
2018-02-12 22:27:20 +0200

    BAP-16535: Remove GenemuFormBundle from dependencies

--- src/Oro/Bundle/CustomerBundle/Tests/Unit/Form/Type/FrontendCustomerTypeAddressTypeTest.php
+++ src/Oro/Bundle/CustomerBundle/Tests/Unit/Form/Type/FrontendCustomerTypeAddressTypeTest.php
@@ -3,7 +3,7 @@
 namespace Oro\Bundle\CustomerBundle\Tests\Unit\Form\Type;
 
 use Doctrine\Common\Collections\ArrayCollection;
-use Genemu\Bundle\FormBundle\Form\JQuery\Type\Select2Type;
+use Oro\Bundle\FormBundle\Form\Type\Select2Type;
 use Oro\Bundle\AddressBundle\Entity\AddressType;
 use Oro\Bundle\CustomerBundle\Entity\Customer;
 use Oro\Bundle\CustomerBundle\Entity\CustomerAddress;
@@ -68,7 +68,10 @@ class FrontendCustomerTypeAddressTypeTest extends CustomerTypedAddressTypeTest
                     ], $this->em),
                     FrontendOwnerSelectTypeStub::NAME => new FrontendOwnerSelectTypeStub(),
                     $addressTypeStub->getName()  => $addressTypeStub,
-                    'genemu_jqueryselect2_translatable_entity' => new Select2Type('translatable_entity'),
+                    'oro_select2_translatable_entity' => new Select2Type(
+                        'Oro\Bundle\TranslationBundle\Form\Type\TranslatableEntityType',
+                        'oro_select2_translatable_entity'
+                    ),
                 ],
                 ['form' => [new StripTagsExtensionStub($this->createMock(HtmlTagHelper::class))]]
             )
