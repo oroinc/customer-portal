1388d3a04c151ae45c1d7f74b9f943bb30f7ab9e
Viacheslav Chekanovskyi <vchekanovskyi@oroinc.com>
2018-04-02 20:38:06 +0300

    BB-7181: Frontend Grid Swipe Actions - move from UIBundle to FrontendBundle

---
+++ src/Oro/Bundle/FrontendBundle/Resources/public/js/app/components/frontend-datagrid-component.js
@@ -0,0 +1,34 @@
+define(function(require) {
+    'use strict';
+
+    var FrontendDataGridComponent;
+    var DataGridComponent = require('orodatagrid/js/app/components/datagrid-component');
+    var ElasticSwipeActionsPlugin = require('orofrontend/js/app/plugins/plugin-elastic-swipe-actions');
+
+    FrontendDataGridComponent = DataGridComponent.extend({
+        /**
+         * @inheritDoc
+         */
+        constructor: function FrontendDataGridComponent() {
+            FrontendDataGridComponent.__super__.constructor.apply(this, arguments);
+        },
+
+        combineGridOptions: function() {
+            var options = FrontendDataGridComponent.__super__.combineGridOptions.apply(this, arguments);
+
+            if (this.metadata.swipeActionsGrid && this.metadata.swipeActionsGrid.enable) {
+                options.plugins.push({
+                    constructor: ElasticSwipeActionsPlugin,
+                    options: {
+                        containerSelector: '.grid-row',
+                        sizerSelector: '.action-cell',
+                        viewport: this.metadata.swipeActionsGrid.viewport || {}
+                    }
+                });
+            }
+
+            return options;
+        }
+    });
+    return FrontendDataGridComponent;
+});
