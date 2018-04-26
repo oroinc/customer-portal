e977c20e26beec366c04958fc5f2d2656374fbdb
Vova Soroka <vsoroka@oroinc.com>
2018-04-26 16:27:09 +0300

    BAP-16937: Owner and Organization validators fail for frontend API (#17931)
    - disable Organization and Owner validators for frontend API
    - add Organization and Owner validators only if appropriate associations are not excluded

---
+++ src/Oro/Bundle/CustomerBundle/Tests/Unit/DependencyInjection/Compiler/FrontendApiPassTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Oro\Bundle\CustomerBundle\Tests\Unit\DependencyInjection\Compiler;
+
+use Oro\Bundle\CustomerBundle\DependencyInjection\Compiler\FrontendApiPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+
+class FrontendApiPassTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var ContainerBuilder */
+    private $container;
+
+    /** @var FrontendApiPass */
+    private $compilerPass;
+
+    protected function setUp()
+    {
+        $this->container = new ContainerBuilder();
+        $this->compilerPass = new FrontendApiPass();
+    }
+
+    /**
+     * @expectedException \Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException
+     * @expectedExceptionMessage non-existent service "oro_api.get_config.add_owner_validator"
+     */
+    public function testProcessWhenAddOwnerValidatorProcessorDoesNotExist()
+    {
+        $this->compilerPass->process($this->container);
+    }
+
+    public function testProcessWhenAddOwnerValidatorProcessorExists()
+    {
+        $definition = $this->container->setDefinition(
+            'oro_api.get_config.add_owner_validator',
+            new Definition()
+        );
+        $definition->addTag('oro.api.processor', []);
+
+        $this->compilerPass->process($this->container);
+
+        self::assertEquals(
+            [
+                ['requestType' => '!frontend']
+            ],
+            $definition->getTag('oro.api.processor')
+        );
+    }
+}
--- src/Oro/Bundle/FrontendBundle/DependencyInjection/Compiler/FrontendApiPass.php
+++ src/Oro/Bundle/FrontendBundle/DependencyInjection/Compiler/FrontendApiPass.php
@@ -2,6 +2,7 @@
 
 namespace Oro\Bundle\FrontendBundle\DependencyInjection\Compiler;
 
+use Oro\Bundle\FrontendBundle\Api\FrontendApiDependencyInjectionUtil;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
@@ -21,27 +22,13 @@ class FrontendApiPass implements CompilerPassInterface
             ['frontend_rest_json_api', new Reference('oro_api.api_doc.formatter.html_formatter')]
         );
 
-        $this->disableApiProcessor($container, 'oro_api.collect_resources.load_dictionaries');
-        $this->disableApiProcessor($container, 'oro_api.collect_resources.load_custom_entities');
-    }
-
-    /**
-     * @param ContainerBuilder $container
-     * @param string           $processorServiceId
-     */
-    private function disableApiProcessor(ContainerBuilder $container, string $processorServiceId)
-    {
-        $processorDef = $container->getDefinition($processorServiceId);
-        $tags = $processorDef->getTag('oro.api.processor');
-        $processorDef->clearTag('oro.api.processor');
-
-        foreach ($tags as $tag) {
-            if (empty($tag['requestType'])) {
-                $tag['requestType'] = '!frontend';
-            } else {
-                $tag['requestType'] .= '&!frontend';
-            }
-            $processorDef->addTag('oro.api.processor', $tag);
-        }
+        FrontendApiDependencyInjectionUtil::disableProcessorForFrontendApi(
+            $container,
+            'oro_api.collect_resources.load_dictionaries'
+        );
+        FrontendApiDependencyInjectionUtil::disableProcessorForFrontendApi(
+            $container,
+            'oro_api.collect_resources.load_custom_entities'
+        );
     }
 }
