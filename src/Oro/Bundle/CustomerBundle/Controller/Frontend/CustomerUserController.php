fe588edf61089e017c1de67fd3a8719af9d51d16
Aleksey Solonenko <asolonenko@oroinc.com>
2018-06-06 19:09:49 +0300

    BB-14324: Consents section is disappears if customer user role has been changed to another (#18521)
    * BB-14324: Consents section is disappears if customer user role has been changed to another
    - add website field to customer user form type

--- src/Oro/Bundle/CustomerBundle/Controller/Frontend/CustomerUserController.php
+++ src/Oro/Bundle/CustomerBundle/Controller/Frontend/CustomerUserController.php
@@ -113,21 +113,9 @@ class CustomerUserController extends Controller
             $this->get('logger')
         );
 
-        $result = $this->get('oro_form.model.update_handler')->handleUpdate(
+        $result = $this->get('oro_form.model.update_handler')->update(
             $customerUser,
             $form,
-            function (CustomerUser $customerUser) {
-                return [
-                    'route' => 'oro_customer_frontend_customer_user_update',
-                    'parameters' => ['id' => $customerUser->getId()]
-                ];
-            },
-            function (CustomerUser $customerUser) {
-                return [
-                    'route' => 'oro_customer_frontend_customer_user_view',
-                    'parameters' => ['id' => $customerUser->getId()]
-                ];
-            },
             $this->get('translator')->trans('oro.customer.controller.customeruser.saved.message'),
             $handler
         );
