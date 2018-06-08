d7d3138832fc8625ec39cbe6a77166b4abd6ecc0
Vladimir Seniuk <vseniuk@oroinc.com>
2018-06-08 16:12:04 +0300

    BAP-16763: RabbitMQ not consuming jobs due to large oro_message_queue_job tables - removed extra changes

--- src/Oro/Bundle/FrontendBundle/Async/Topics.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Oro\Bundle\FrontendBundle\Async;
-
-final class Topics
-{
-    const PARENT_MESSAGE_TOPIC = 'demo.message_queue_examples.parent_message';
-    const CHILD_MESSAGE_TOPIC = 'demo.message_queue_examples.child_message';
-}
