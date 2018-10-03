ccc4ff2199864441f5cc80e54db90e25b6c966e7
Viacheslav Bemza <vbemza@oroinc.com>
2018-10-03 18:17:35 +0300

    BB-15252: Check that not usable option is hidden in attribute filter - fix behat scenarios

--- src/Oro/Bundle/CustomerBundle/Tests/Behat/Context/FrontendGridContext.php
+++ src/Oro/Bundle/CustomerBundle/Tests/Behat/Context/FrontendGridContext.php
@@ -138,7 +138,7 @@ class FrontendGridContext extends OroFeatureContext implements OroPageObjectAwar
     public function assertHasFilterInManagerInFrontendGrid(string $filter, string $dataGridName = null)
     {
         /** @var FrontendGridFilterManager $filterManager */
-        $filterManager = $this->getFilterManager($dataGridName);
+        $filterManager = $this->getFilterSelector($dataGridName);
         self::assertTrue(
             $filterManager->hasFilter($filter),
             sprintf('Filter %s is not present in %s grid filter manager', $filter, $dataGridName)
@@ -160,7 +160,7 @@ class FrontendGridContext extends OroFeatureContext implements OroPageObjectAwar
     public function assertHasNoFilterInManagerInFrontendGrid(string $filter, string $dataGridName = null)
     {
         /** @var FrontendGridFilterManager $filterManager */
-        $filterManager = $this->getFilterManager($dataGridName);
+        $filterManager = $this->getFilterSelector($dataGridName);
         self::assertFalse(
             $filterManager->hasFilter($filter),
             sprintf('Filter %s is present in %s grid filter manager', $filter, $dataGridName)
@@ -228,7 +228,6 @@ class FrontendGridContext extends OroFeatureContext implements OroPageObjectAwar
         return $manager;
     }
 
-
     /**
      * @param null $datagridName
      *
@@ -243,5 +242,21 @@ class FrontendGridContext extends OroFeatureContext implements OroPageObjectAwar
         $filterButton->click();
 
         return $grid->getElement($grid->getMappedChildElementName('FrontendGridFilterManager'));
+    }
+
+    /**
+     * @param null $datagridName
+     *
+     * @return FrontendGridFilterManager|Element
+     */
+    protected function getFilterSelector($datagridName = null)
+    {
+        $grid = $this->getFrontendGrid($datagridName);
+
+        $grid->getElement($grid->getMappedChildElementName('GridFiltersButton'))->open();
+        $filterButton = $grid->getElement($grid->getMappedChildElementName('GridFilterManagerButton'));
+        $filterButton->click();
+
+        return $grid->getElement($grid->getMappedChildElementName('FrontendGridFilterSelector'));
     }
 }
