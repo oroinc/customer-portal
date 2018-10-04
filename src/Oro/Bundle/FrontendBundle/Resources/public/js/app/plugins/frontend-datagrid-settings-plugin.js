23691a993dce6c05aec1701e9794b92ab8b58603
Viacheslav Chekanovskiy <vchekanovskyi@oroinc.com>
2018-10-04 12:44:39 +0300

    BAP-17198: Move filter manager into grid's settings (#19912)
    BAP-17198: Move filter manager into grid's settings
    - move filter manager to grid settings
    - refactoring grid settings
    - create grid settings tabs
    - refactoring column manager plugin
    - create front end column manager plugin
    - rename ColumnManager view to DatagridSettingsList
    - add Select All/Unselect All for filter manager
    - create datagrid settings with dialog widget in mobile view
    - change namespace for datagrid settings from "column-manager" to "datagrid-settings"
    - add needed  alias to requirejs config

---
+++ src/Oro/Bundle/FrontendBundle/Resources/public/js/app/plugins/frontend-datagrid-settings-plugin.js
@@ -0,0 +1,35 @@
+define(function(require) {
+    'use strict';
+
+    var FrontendDatagridSettingsPlugin;
+    var _ = require('underscore');
+    var ShowComponentAction = require('oro/datagrid/action/show-component-action');
+    var DatagridSettingsPlugin = require('orodatagrid/js/app/plugins/grid/datagrid-settings-plugin');
+    var DatagridSettingView = require('orodatagrid/js/app/views/grid/datagrid-settings-view');
+
+    var config = require('module').config();
+    config = _.extend({
+        icon: 'cog',
+        wrapperClassName: 'datagrid-settings',
+        label: _.__('oro.datagrid.column_manager.title')
+    }, config);
+
+    FrontendDatagridSettingsPlugin = DatagridSettingsPlugin.extend({
+        onBeforeToolbarInit: function(toolbarOptions) {
+            var options = {
+                datagrid: this.main,
+                launcherOptions: _.extend(config, {
+                    componentConstructor: toolbarOptions.componentConstructor || DatagridSettingView,
+                    columns: this.main.columns,
+                    collection: this.main.columns,
+                    allowDialog: false
+                }, toolbarOptions.datagridSettings),
+                order: 600
+            };
+
+            toolbarOptions.addToolbarAction(new ShowComponentAction(options));
+        }
+    });
+
+    return FrontendDatagridSettingsPlugin;
+});
