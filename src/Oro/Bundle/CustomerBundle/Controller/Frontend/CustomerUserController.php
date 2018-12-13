b0e0fd10c5889e5ef0af88fee0b8c1d6ee5b0c8f
Eugene Chetverikov <EugeneC@users.noreply.github.com>
2018-12-13 14:52:36 +0200

    BB-15768: Fixing failed tests (#21299)
    * BB-15768: Fixing failed tests
    - missing doc comments have been added for the modified classes
    - ConsentContextProvider has been updated in order to use current website from website manager
    - artefacts have been removed

--- src/Oro/Bundle/CustomerBundle/Controller/Frontend/CustomerUserController.php
+++ src/Oro/Bundle/CustomerBundle/Controller/Frontend/CustomerUserController.php
@@ -13,6 +13,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * CRUD controller for CustomerUser entity
+ */
 class CustomerUserController extends Controller
 {
     /**
