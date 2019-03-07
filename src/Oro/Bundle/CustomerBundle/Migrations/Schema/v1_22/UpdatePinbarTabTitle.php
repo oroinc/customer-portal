41c846fa77a53d34959067ed625c6ab4e037a329
Dmitriy Pasechnik <dpasechnik@oroinc.com>
2019-03-07 13:58:46 +0200

    BAP-18448: Migration failed on 3.1 (#22872)
    BAP-18448: Migration failed on 3.1

--- src/Oro/Bundle/CustomerBundle/Migrations/Schema/v1_22/UpdatePinbarTabTitle.php
+++ src/Oro/Bundle/CustomerBundle/Migrations/Schema/v1_22/UpdatePinbarTabTitle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Oro\Bundle\CustomerBundle\Migrations\Schema\v1_21;
+namespace Oro\Bundle\CustomerBundle\Migrations\Schema\v1_22;
 
 use Doctrine\DBAL\Schema\Schema;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
