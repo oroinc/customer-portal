424bc30d14bcd0d359fb3cc608d5968b9a5db345
Vitalii Yeromenko <vyeromenko@magecore.com>
2019-03-19 17:11:51 +0200

    BB-16409: Incorrect recipient in Send Mail widget on order view page (#22821)
    * BB-16409 Incorrect recipient in Send Mail widget on order view page
    - added the interface for getting full name associated with email address
    - passing formatted email address with full name into Send email button instead of email only
    - add behat scenario for checking recipient full name when creating email from an Order
    - set CustomerUser as email owner
    - add customer user to recipients section in email activity
    - fixed recursion depth for related emails provider
    - refactored RelatedEmailsProvider

---
+++ src/Oro/Bundle/CustomerBundle/Tests/Unit/Provider/EmailRecipientsProviderTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Oro\Bundle\CustomerBundle\Tests\Unit\Provider;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Oro\Bundle\CustomerBundle\Entity\CustomerUser;
+use Oro\Bundle\CustomerBundle\Entity\Repository\CustomerUserRepository;
+use Oro\Bundle\CustomerBundle\Provider\EmailRecipientsProvider;
+use Oro\Bundle\EmailBundle\Model\EmailRecipientsProviderArgs;
+use Oro\Bundle\EmailBundle\Provider\EmailRecipientsHelper;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class EmailRecipientsProviderTest extends TestCase
+{
+    /**
+     * @var Registry|MockObject
+     */
+    protected $registry;
+
+    /**
+     * @var EmailRecipientsHelper|MockObject
+     */
+    protected $emailRecipientsHelper;
+
+    /**
+     * @var EmailRecipientsProvider|MockObject
+     */
+    protected $emailRecipientsProvider;
+
+    public function setUp()
+    {
+        $this->registry = $this->getMockBuilder(Registry::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->emailRecipientsHelper = $this->getMockBuilder(EmailRecipientsHelper::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->emailRecipientsProvider = new EmailRecipientsProvider(
+            $this->registry,
+            $this->emailRecipientsHelper
+        );
+    }
+
+    /**
+     * @dataProvider dataProvider
+     *
+     * @param EmailRecipientsProviderArgs $args
+     * @param array $recipients
+     */
+    public function testGetRecipients(EmailRecipientsProviderArgs $args, array $recipients)
+    {
+        $customerUserRepository = $this->getMockBuilder(CustomerUserRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->registry->expects($this->once())
+            ->method('getRepository')
+            ->with(CustomerUser::class)
+            ->willReturn($customerUserRepository);
+
+        $this->emailRecipientsHelper->expects($this->once())
+            ->method('getRecipients')
+            ->with($args, $customerUserRepository, 'cu', CustomerUser::class)
+            ->willReturn($recipients);
+
+        $this->assertEquals($recipients, $this->emailRecipientsProvider->getRecipients($args));
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProvider()
+    {
+        return [
+            [
+                new EmailRecipientsProviderArgs(null, null, 1),
+                [
+                    'recipient@example.com'  => 'Recipient <recipient@example.com>',
+                ],
+            ],
+        ];
+    }
+}
