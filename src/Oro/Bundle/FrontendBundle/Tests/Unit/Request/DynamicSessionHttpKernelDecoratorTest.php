4a60c90dbea4b53e58422795b977b1da1f7f7573
Yurii Muratov <ymuratov@oroinc.com>
2019-04-19 21:00:51 +0300

    BB-16592: Can't login to the management console when application installed to subfolder (#23525)

--- src/Oro/Bundle/FrontendBundle/Tests/Unit/Request/DynamicSessionHttpKernelDecoratorTest.php
+++ src/Oro/Bundle/FrontendBundle/Tests/Unit/Request/DynamicSessionHttpKernelDecoratorTest.php
@@ -195,4 +195,134 @@ class DynamicSessionHttpKernelDecoratorTest extends \PHPUnit\Framework\TestCase
             $this->kernelDecorator
         );
     }
+
+    public function testHandleForBackendRequestForApplicationInSubDir()
+    {
+        $request = $this->createMock(Request::class);
+        $request->expects(self::once())
+            ->method('getBasePath')
+            ->willReturn('/subDir');
+        $type = HttpKernelInterface::MASTER_REQUEST;
+        $catch = true;
+        $response = $this->createMock(Response::class);
+        $this->frontendHelper->expects(self::once())
+            ->method('isFrontendRequest')
+            ->with(self::identicalTo($request))
+            ->willReturn(false);
+        $this->kernel->expects(self::once())
+            ->method('handle')
+            ->with(self::identicalTo($request), $type, $catch)
+            ->willReturn($response);
+        self::assertSame(
+            $response,
+            $this->kernelDecorator->handle($request, $type, $catch)
+        );
+        $this->assertEquals(
+            '/admin',
+            $this->container->getParameter('session.storage.options')['cookie_path']
+        );
+        self::assertAttributeSame(
+            null,
+            'backendSessionOptions',
+            $this->kernelDecorator
+        );
+    }
+
+    public function testHandleForFrontendRequestForApplicationInSubDir()
+    {
+        $request = $this->createMock(Request::class);
+        $request->expects(self::once())
+            ->method('getBasePath')
+            ->willReturn('/subDir');
+        $type = HttpKernelInterface::MASTER_REQUEST;
+        $catch = true;
+        $response = $this->createMock(Response::class);
+        $this->frontendHelper->expects(self::once())
+            ->method('isFrontendRequest')
+            ->with(self::identicalTo($request))
+            ->willReturn(true);
+        $this->kernel->expects(self::once())
+            ->method('handle')
+            ->with(self::identicalTo($request), $type, $catch)
+            ->willReturn($response);
+        self::assertSame(
+            $response,
+            $this->kernelDecorator->handle($request, $type, $catch)
+        );
+        self::assertEquals(
+            [
+                'name' => 'FRONT',
+                'cookie_path' => '/subDir/',
+                'cookie_lifetime' => 10
+            ],
+            $this->container->getParameter('session.storage.options')
+        );
+        self::assertAttributeEquals(
+            self::BACKEND_SESSION_OPTIONS,
+            'backendSessionOptions',
+            $this->kernelDecorator
+        );
+    }
+
+    public function testHandleForBackendRequestAfterFrontendRequestForApplicationInSubDir()
+    {
+        $request = $this->createMock(Request::class);
+        $request->expects(self::exactly(2))
+            ->method('getBasePath')
+            ->willReturn('/subDir');
+        $this->frontendHelper->expects(self::exactly(2))
+            ->method('isFrontendRequest')
+            ->willReturnOnConsecutiveCalls(true, false);
+        $this->kernel->expects(self::exactly(2))
+            ->method('handle')
+            ->willReturnOnConsecutiveCalls($this->createMock(Response::class), $this->createMock(Response::class));
+        // frontend request
+        $this->kernelDecorator->handle($request);
+        // backend request
+        $this->kernelDecorator->handle($request);
+        self::assertEquals(
+            [
+                'name' => 'BACK',
+                'cookie_path' => '/admin',
+                'cookie_lifetime' => 10
+            ],
+            $this->container->getParameter('session.storage.options')
+        );
+        self::assertAttributeEquals(
+            self::BACKEND_SESSION_OPTIONS,
+            'backendSessionOptions',
+            $this->kernelDecorator
+        );
+    }
+
+    public function testHandleForFrontendRequestAfterBackendRequestForApplicationInSubDir()
+    {
+        $request = $this->createMock(Request::class);
+        $request->expects(self::exactly(2))
+            ->method('getBasePath')
+            ->willReturn('/subDir');
+        $this->frontendHelper->expects(self::exactly(2))
+            ->method('isFrontendRequest')
+            ->willReturnOnConsecutiveCalls(false, true);
+        $this->kernel->expects(self::exactly(2))
+            ->method('handle')
+            ->willReturnOnConsecutiveCalls($this->createMock(Response::class), $this->createMock(Response::class));
+        // backend request
+        $this->kernelDecorator->handle($request);
+        // frontend request
+        $this->kernelDecorator->handle($request);
+        self::assertEquals(
+            [
+                'name' => 'FRONT',
+                'cookie_path' => '/subDir/',
+                'cookie_lifetime' => 10
+            ],
+            $this->container->getParameter('session.storage.options')
+        );
+        self::assertAttributeEquals(
+            self::BACKEND_SESSION_OPTIONS,
+            'backendSessionOptions',
+            $this->kernelDecorator
+        );
+    }
 }
