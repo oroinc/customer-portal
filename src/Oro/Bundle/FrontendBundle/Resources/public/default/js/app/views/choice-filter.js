7b6f61091a4cdcafd21004600ef3bac6145241ba
Dmitrii Shykalenko <dshykalenko@oroinc.com>
2019-04-22 13:06:38 +0300

    BB-16512: Filters "does not contain" and "is equal to" don't work (#23415)
    * BB-16512: Filters "does not contain" and "is equal to" don't work
    - fix date range filter
    - fixed review comments
    - fixed price filter on frontend

--- src/Oro/Bundle/FrontendBundle/Resources/public/default/js/app/views/choice-filter.js
+++ src/Oro/Bundle/FrontendBundle/Resources/public/default/js/app/views/choice-filter.js
@@ -2,13 +2,19 @@ define(function(require) {
     'use strict';
 
     var ChoiceFilter;
+    var _ = require('underscore');
     var BaseChoiceFilter = require('oro/filter/choice-filter');
 
     ChoiceFilter = BaseChoiceFilter.extend({
-        choiceSelectSelector: 'select[data-choice-value-select]',
+        /**
+         * @inheritDoc
+         */
+        criteriaValueSelectors: _.defaults({
+            type: 'select[data-choice-value-select]'
+        }, BaseChoiceFilter.prototype.criteriaValueSelectors),
 
         events: {
-            'change select[data-choice-value-select]': '_onClickChoiceValue'
+            'change select[data-choice-value-select]': '_onChangeChoiceValue'
         },
 
         /**
@@ -18,24 +24,34 @@ define(function(require) {
             ChoiceFilter.__super__.constructor.apply(this, arguments);
         },
 
+        _renderCriteria: function() {
+            ChoiceFilter.__super__._renderCriteria.call(this);
+
+            this.$el.inputWidget('seekAndCreate');
+        },
+
         /**
          * @inheritDoc
          */
-        _onClickChoiceValue: function(e) {
-            var type = e.currentTarget.value;
-            this._onClickChoiceValueSetType(type);
-            this._updateValueField();
+        _onChangeChoiceValue: function(e) {
+            if (!this.changeChoiceValueHandling) {
+                this.changeChoiceValueHandling = true;
+                this._onClickChoiceValueSetType(e.currentTarget.value);
+                this._updateValueField();
+                delete this.changeChoiceValueHandling;
+            }
         },
 
         /**
          * @inheritDoc
          */
         _onValueUpdated: function(newValue, oldValue) {
-            this.$(this.choiceSelectSelector).each(function(i, elem) {
+            this.$(this.criteriaValueSelectors.type).each(function(i, elem) {
                 var $select = this.$(elem);
                 var name = $select.data('name') || 'type';
                 if (oldValue[name] !== newValue[name]) {
-                    $select.val(newValue[name]).trigger('change');
+                    $select.inputWidget('val', newValue[name]);
+                    $select.trigger('change');
                 }
             }.bind(this));
 
