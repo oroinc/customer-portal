b00b3803685c3394ead652116ae8040796fb37e9
Vladimir Seniuk <vseniuk@oroinc.com>
2019-09-20 16:16:06 +0300

    BB-17578: Backmerge GrapesJS frontend story - collect grapesjs themes from form type options

--- src/Oro/Bundle/FrontendBundle/Tests/Unit/Form/Extension/WYSIWYGTypeExtensionTest.php
+++ src/Oro/Bundle/FrontendBundle/Tests/Unit/Form/Extension/WYSIWYGTypeExtensionTest.php
@@ -7,11 +7,9 @@ use Oro\Bundle\ConfigBundle\Config\ConfigManager;
 use Oro\Bundle\FrontendBundle\Form\Extension\WYSIWYGTypeExtension;
 use Oro\Bundle\WebsiteBundle\Manager\WebsiteManager;
 use Oro\Component\Layout\Extension\Theme\DataProvider\ThemeProvider;
-use Oro\Component\Layout\Extension\Theme\Model\Theme;
 use Oro\Component\Layout\Extension\Theme\Model\ThemeManager;
 use Oro\Component\Testing\Unit\EntityTrait;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class WYSIWYGTypeExtensionTest extends \PHPUnit\Framework\TestCase
 {
@@ -62,64 +60,17 @@ class WYSIWYGTypeExtensionTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals([WYSIWYGType::class], $this->extension::getExtendedTypes());
     }
 
-    public function testFinishView(): void
+    public function testConfigureOptions()
     {
-        $theme1 = new Theme('theme1');
-        $theme1->setLabel('label1');
-        $theme2 = new Theme('theme2');
-        $theme2->setLabel('label2');
-        $theme3 = new Theme('theme3');
-        $theme3->setLabel('label3');
-        $this->themeManager->expects($this->once())
-            ->method('getAllThemes')
-            ->willReturn([$theme1, $theme2, $theme3]);
+        /* @var $resolver OptionsResolver|\PHPUnit\Framework\MockObject\MockObject */
+        $resolver = $this->createMock(OptionsResolver::class);
+        $resolver->expects($this->once())
+            ->method('setDefault')
+            ->with('page-component', function() {
 
-        $this->themeProvider->expects($this->exactly(3))
-            ->method('getStylesOutput')
-            ->withConsecutive(
-                [$theme1->getName()],
-                [$theme2->getName()],
-                [$theme3->getName()]
-            )->willReturnOnConsecutiveCalls(
-                '/path/to/theme1',
-                '/path/to/theme2',
-                '/path/to/theme3'
-            );
+            })
+            ->will($this->returnSelf());
 
-
-        $this->configManager->expects($this->once())
-            ->method('get')
-            ->with('oro_frontend.frontend_theme', false, false, null)
-            ->willReturn($theme2->getName());
-
-        $view = new FormView();
-        /** @var FormInterface|\PHPUnit\Framework\MockObject\MockObject $form */
-        $form = $this->createMock(FormInterface::class);
-        $this->extension->finishView($view, $form, []);
-
-        $expectedOptions = [
-            'themes' => [
-                [
-                    'name' => $theme1->getName(),
-                    'label' => $theme1->getLabel(),
-                    'stylesheet' => '/path/to/theme1',
-                ],
-                [
-                    'name' => $theme2->getName(),
-                    'label' => $theme2->getLabel(),
-                    'active' => true,
-                    'stylesheet' => '/path/to/theme2',
-                ],
-                [
-                    'name' => $theme3->getName(),
-                    'label' => $theme3->getLabel(),
-                    'stylesheet' => '/path/to/theme3',
-                ],
-            ]
-        ];
-        $this->assertEquals(
-            $expectedOptions,
-            json_decode($view->vars['attr']['data-page-component-options'], \JSON_OBJECT_AS_ARRAY)
-        );
+        $this->extension->configureOptions($resolver);
     }
 }
