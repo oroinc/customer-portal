4e292696954b0429c00ad836cdc7a470edaf4518
Viacheslav Bemza <vbemza@oroinc.com>
2019-10-01 17:47:51 +0300

    BB-17341: Customer Hierarchy Navigation (#24953)
    - added parent chain and subsidiaries grid
    - covered by unit and behat tests
    - increase total queries limit for customer view page because of add new grid + account data information
    - created collapse-view styles for Management Console

--- src/Oro/Bundle/CustomerBundle/Twig/CustomerExtension.php
+++ src/Oro/Bundle/CustomerBundle/Twig/CustomerExtension.php
@@ -2,9 +2,17 @@
 
 namespace Oro\Bundle\CustomerBundle\Twig;
 
+use Oro\Bundle\CustomerBundle\Entity\Customer;
 use Oro\Bundle\CustomerBundle\Security\CustomerUserProvider;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
+/**
+ * Provides a Twig function to check if custom user has access to view an entity:
+ *   - is_granted_view_customer_user
+ * Provides a Twig function to build customer parent chain:
+ *   - oro_customer_parent_parts
+ */
 class CustomerExtension extends \Twig_Extension
 {
     const NAME = 'customer_extension';
@@ -29,13 +37,46 @@ class CustomerExtension extends \Twig_Extension
     }
 
     /**
+     * @return LoggerInterface
+     */
+    protected function getLogger()
+    {
+        return $this->container->get('logger');
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function getFunctions()
     {
         return [
             new \Twig_SimpleFunction('is_granted_view_customer_user', [$this, 'isGrantedViewCustomerUser']),
+            new \Twig_SimpleFunction('oro_customer_parent_parts', [$this, 'getCustomerParentParts']),
         ];
+    }
+    /**
+     * @param Customer $customer
+     * @return array
+     */
+    public function getCustomerParentParts(Customer $customer): array
+    {
+        $parts = [];
+        $i = 0;
+        while ($customer->getParent() && $customer->getParent()->getName()) {
+            if ($i++ > 50) {
+                $this->getLogger()->warning(
+                    sprintf('Customer parent loop limit was reached for customer #%s.', $customer->getId())
+                );
+                break;
+            }
+            $customer = $customer->getParent();
+            $parts[] = [
+                'name' => $customer->getName(),
+                'id' => $customer->getId(),
+            ];
+        }
+
+        return array_reverse($parts);
     }
 
     /**
