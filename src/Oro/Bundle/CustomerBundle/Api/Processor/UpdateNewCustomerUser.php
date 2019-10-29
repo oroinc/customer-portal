c43a21e7e29838cde413edd7834ee010362129fe
Vova Soroka <vsoroka@oroinc.com>
2019-10-29 14:29:24 +0200

    BB-11791: Possibility to create included CustomerUser using Customer Rest API (#25530)
    - fix CustomerUser API
    - fix User API
    - fix PriceRule API
    - fix PriceListSchedule API
    - fix PriceList API

--- src/Oro/Bundle/CustomerBundle/Api/Processor/UpdateNewCustomerUser.php
+++ src/Oro/Bundle/CustomerBundle/Api/Processor/UpdateNewCustomerUser.php
@@ -1,17 +1,18 @@
 <?php
 
-namespace Oro\Bundle\CustomerBundle\Api\Processor\Create;
+namespace Oro\Bundle\CustomerBundle\Api\Processor;
 
-use Oro\Bundle\ApiBundle\Processor\Create\CreateContext;
+use Oro\Bundle\ApiBundle\Form\FormUtil;
+use Oro\Bundle\ApiBundle\Processor\CustomizeFormData\CustomizeFormDataContext;
 use Oro\Bundle\CustomerBundle\Entity\CustomerUser;
 use Oro\Bundle\CustomerBundle\Entity\CustomerUserManager;
 use Oro\Component\ChainProcessor\ContextInterface;
 use Oro\Component\ChainProcessor\ProcessorInterface;
 
 /**
- * Saves new CustomerUser entity to the database.
+ * Prepares a new CustomerUser entity to be saved to the database.
  */
-class SaveCustomerUser implements ProcessorInterface
+class UpdateNewCustomerUser implements ProcessorInterface
 {
     /** @var CustomerUserManager */
     private $userManager;
@@ -29,20 +30,21 @@ class SaveCustomerUser implements ProcessorInterface
      */
     public function process(ContextInterface $context)
     {
-        /** @var CreateContext $context */
+        /** @var CustomizeFormDataContext $context */
 
-        /** @var CustomerUser $user */
-        $user = $context->getResult();
-        if (!is_object($user)) {
-            // entity does not exist
+        $form = $context->getForm();
+        if (!FormUtil::isSubmittedAndValid($form)) {
             return;
         }
 
+        /** @var CustomerUser $user */
+        $user = $form->getData();
+
         // generate random secure password for a user
         if (!$user->getPlainPassword()) {
-            $user->setPlainPassword($this->userManager->generatePassword(30));
+            $user->setPlainPassword($this->userManager->generatePassword());
         }
 
-        $this->userManager->updateUser($user);
+        $this->userManager->updateUser($user, false);
     }
 }
