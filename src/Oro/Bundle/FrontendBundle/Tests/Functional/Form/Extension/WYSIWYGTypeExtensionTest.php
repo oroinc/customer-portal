f206ac5ec58cc989e4dce3507f2d85a44571e7fa
Vladimir Seniuk <vladimirseniuk@users.noreply.github.com>
2019-10-29 09:59:15 +0200

    BB-17700: Import validation (#25576)
    - Add WYSIWYG validation to all extend fields with type WYSIWYG;
    - Works for form, API and import validation.

--- src/Oro/Bundle/FrontendBundle/Tests/Functional/Form/Extension/WYSIWYGTypeExtensionTest.php
+++ src/Oro/Bundle/FrontendBundle/Tests/Functional/Form/Extension/WYSIWYGTypeExtensionTest.php
@@ -2,6 +2,7 @@
 
 namespace Oro\Bundle\FrontendBundle\Tests\Functional\Form\Extension;
 
+use Oro\Bundle\CMSBundle\Entity\Page;
 use Oro\Bundle\FrontendBundle\Tests\Functional\Form\Extension\Stub\PageTypeStub;
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
 
@@ -10,17 +11,16 @@ class WYSIWYGTypeExtensionTest extends WebTestCase
     protected function setUp()
     {
         $this->initClient();
+        $this->updateUserSecurityToken(self::AUTH_USER);
     }
 
     public function testFinishView(): void
     {
         $container = $this->getContainer();
 
-        $form = $container->get('form.factory')->create(PageTypeStub::class);
+        $form = $container->get('form.factory')->create(PageTypeStub::class, null, ['data_class' => Page::class]);
         $fieldView = $form->get('content')->createView();
         $actualOptions = json_decode($fieldView->vars['attr']['data-page-component-options'], \JSON_OBJECT_AS_ARRAY);
-
-        $themeManager = $container->get('oro_layout.theme_manager');
 
         $this->assertArrayHasKey('themes', $actualOptions);
         $this->assertIsArray($actualOptions['themes']);
