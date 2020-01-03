8102d3eee9e56ca3e5855fa89eb66819fbf8a131
Yefim Y <webevt@users.noreply.github.com>
2020-01-03 14:58:49 +0200

    BB-18585: Add behat tests (#26359)
    - added behat test
    - added delete menu update cache when content node is deleted

---
+++ src/Oro/Bundle/CommerceMenuBundle/EventListener/ContentNodeDeleteListener.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Oro\Bundle\CommerceMenuBundle\EventListener;
+
+use Doctrine\Common\Cache\CacheProvider;
+
+/**
+ * Deletes menuUpdate query cache when ContentNode is deleted.
+ */
+class ContentNodeDeleteListener
+{
+    /** @var CacheProvider */
+    private $cacheProvider;
+
+    /**
+     * @param CacheProvider $cacheProvider
+     */
+    public function __construct(CacheProvider $cacheProvider)
+    {
+        $this->cacheProvider = $cacheProvider;
+    }
+
+    public function postRemove(): void
+    {
+        $this->cacheProvider->deleteAll();
+    }
+}
