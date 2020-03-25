276a22de31c36c9ef8d60a2805844a2462a75a12
Viacheslav Bemza <vbemza@oroinc.com>
2020-03-25 20:00:09 +0200

    BB-19003: Incorrect routing for media in multi websites configuration  (#27171)

---
+++ src/Oro/Bundle/WebsiteBundle/Tests/Unit/Asset/RequestContextTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Oro\Bundle\WebsiteBundle\Tests\Unit\Asset;
+
+use Oro\Bundle\WebsiteBundle\Asset\BasePathResolver;
+use Oro\Bundle\WebsiteBundle\Asset\RequestContext;
+
+class RequestContextTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var BasePathResolver|\PHPUnit\Framework\MockObject\MockObject
+     */
+    private $resolver;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $this->resolver = $this->createMock(BasePathResolver::class);
+    }
+
+    public function testGetBaseUrl()
+    {
+        $path = '/path';
+        $expected = '/resolved-path';
+        $context = new RequestContext($path);
+        $context->setBasePathResolver($this->resolver);
+        $this->resolver->expects($this->once())
+            ->method('resolveBasePath')
+            ->with($path)
+            ->willReturn($expected);
+
+        self::assertEquals($expected, $context->getBaseUrl());
+    }
+}
