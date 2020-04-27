a44f22f591eff5d69c483f0d43fff08fd0c0866a
Yefim Yevtushenko <webevt@users.noreply.github.com>
2020-04-28 01:31:40 +0300

    BB-19150: Sitemap generation memory leak (4.1) (#27567)
    - added memory cache to WebsiteUrlResolver
    - added BufferedQueryWithDoctrineIterableResultIterator
    - made use of new iterator in UrlItemsProvider

--- src/Oro/Bundle/WebsiteBundle/Tests/Unit/Resolver/WebsiteUrlResolverTest.php
+++ src/Oro/Bundle/WebsiteBundle/Tests/Unit/Resolver/WebsiteUrlResolverTest.php
@@ -2,15 +2,21 @@
 
 namespace Oro\Bundle\WebsiteBundle\Tests\Unit\Resolver;
 
+use Oro\Bundle\CacheBundle\Tests\Unit\Provider\MemoryCacheProviderAwareTestTrait;
 use Oro\Bundle\ConfigBundle\Config\ConfigManager;
 use Oro\Bundle\WebsiteBundle\Entity\Website;
 use Oro\Bundle\WebsiteBundle\Resolver\WebsiteUrlResolver;
 use Oro\Component\Testing\Unit\EntityTrait;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
+/**
+ * @SuppressWarnings(PHPMD.TooManyPublicMethods)
+ * @SuppressWarnings(PHPMD.TooManyMethods)
+ */
 class WebsiteUrlResolverTest extends \PHPUnit\Framework\TestCase
 {
     use EntityTrait;
+    use MemoryCacheProviderAwareTestTrait;
 
     private const CONFIG_URL        = 'oro_website.url';
     private const CONFIG_SECURE_URL = 'oro_website.secure_url';
@@ -46,6 +52,30 @@ class WebsiteUrlResolverTest extends \PHPUnit\Framework\TestCase
         $this->assertSame($url, $this->websiteUrlResolver->getWebsiteUrl($website));
     }
 
+    public function testGetWebsiteUrlWhenMemoryCacheProvider(): void
+    {
+        $this->mockMemoryCacheProvider();
+        $this->setMemoryCacheProvider($this->websiteUrlResolver);
+
+        $this->testGetWebsiteUrl();
+    }
+
+    public function testGetWebsiteUrlWhenCache(): void
+    {
+        $url = 'http://global.website.url/';
+
+        /** @var Website $website */
+        $website = $this->getEntity(Website::class, ['id' => 2]);
+
+        $this->configManager
+            ->expects($this->never())
+            ->method('get');
+
+        $this->mockMemoryCacheProvider($url);
+        $this->setMemoryCacheProvider($this->websiteUrlResolver);
+        $this->assertSame($url, $this->websiteUrlResolver->getWebsiteUrl($website));
+    }
+
     public function testGetWebsiteSecureUrlHasSecureUrl()
     {
         $url = 'https://website.url/';
@@ -60,6 +90,31 @@ class WebsiteUrlResolverTest extends \PHPUnit\Framework\TestCase
             ->method('get')
             ->with(self::CONFIG_SECURE_URL, false, true, $website)
             ->willReturn($urlConfig);
+
+        $this->assertSame($url, $this->websiteUrlResolver->getWebsiteSecureUrl($website));
+    }
+
+    public function testGetWebsiteSecureUrlHasSecureUrlWhenMemoryCacheProvider(): void
+    {
+        $this->mockMemoryCacheProvider();
+        $this->setMemoryCacheProvider($this->websiteUrlResolver);
+
+        $this->testGetWebsiteSecureUrlHasSecureUrl();
+    }
+
+    public function testGetWebsiteSecureUrlHasSecureUrlWhenCache(): void
+    {
+        $url = 'https://website.url/';
+
+        /** @var Website $website */
+        $website = $this->getEntity(Website::class, ['id' => 2]);
+
+        $this->configManager
+            ->expects($this->never())
+            ->method('get');
+
+        $this->mockMemoryCacheProvider($url);
+        $this->setMemoryCacheProvider($this->websiteUrlResolver);
 
         $this->assertSame($url, $this->websiteUrlResolver->getWebsiteSecureUrl($website));
     }
