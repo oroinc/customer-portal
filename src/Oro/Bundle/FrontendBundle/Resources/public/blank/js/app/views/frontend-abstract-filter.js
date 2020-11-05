a4d56776a30e1c4657f909341860e04603cc4ee0
Alex Yegerev <94-lexxcode@users.noreply.git.oroinc.com>
2020-11-05 16:33:00 +0000

    BB-19993: CSS issues - Fixed CSS issues - Add collapse animation

---
+++ src/Oro/Bundle/FrontendBundle/Resources/public/blank/js/app/views/frontend-abstract-filter.js
@@ -0,0 +1,53 @@
+define(function(require, exports, module) {
+    'use strict';
+
+    const _ = require('underscore');
+    const AbstractFilter = require('oro/filter/abstract-filter');
+
+    let config = require('module-config').default(module.id);
+
+    config = _.extend({
+        animationDuration: 0
+    }, config);
+
+    const FrontendAbstractFilter = AbstractFilter.extend({
+        /**
+         * Duration of slide up/down filter criteria
+         *
+         * @property {Number}
+         */
+        animationDuration: config.animationDuration,
+
+        /**
+         * @inheritDoc
+         */
+        constructor: function FrontendAbstractFilter(options) {
+            FrontendAbstractFilter.__super__.constructor.call(this, options);
+        },
+
+        /**
+         * Set filter button class
+         *
+         * @param {Object} element
+         * @param {Boolean} status
+         * @protected
+         */
+        _setButtonPressed: function(element, status) {
+            if (!this.animationDuration) {
+                return FrontendAbstractFilter.__super__._setButtonPressed.call(this, element, status);
+            }
+
+            if (status) {
+                element.slideDown(this.animationDuration, () => {
+                    element.parent().addClass(this.buttonActiveClass);
+                });
+            } else {
+                element.slideUp(this.animationDuration, () => {
+                    element.parent().removeClass(this.buttonActiveClass);
+                });
+            }
+        }
+    });
+
+    return FrontendAbstractFilter;
+});
