e468513695cfce989018df7080b354b4c01c9d5d
Vova Soroka <vsoroka@oroinc.com>
2021-05-12 11:37:12 +0300

    BB-20145: Visitor cannot add Order via API due to lack of permissions (#29725)

--- src/Oro/Bundle/CustomerBundle/Tests/Unit/Entity/GuestCustomerUserManagerTest.php
+++ src/Oro/Bundle/CustomerBundle/Tests/Unit/Entity/GuestCustomerUserManagerTest.php
@@ -3,33 +3,35 @@
 namespace Oro\Bundle\CustomerBundle\Tests\Unit\Entity;
 
 use Oro\Bundle\CustomerBundle\Entity\CustomerGroup;
+use Oro\Bundle\CustomerBundle\Entity\CustomerUser;
 use Oro\Bundle\CustomerBundle\Entity\CustomerUserManager;
 use Oro\Bundle\CustomerBundle\Entity\CustomerUserRole;
 use Oro\Bundle\CustomerBundle\Entity\GuestCustomerUserManager;
 use Oro\Bundle\CustomerBundle\Provider\CustomerUserRelationsProvider;
-use Oro\Bundle\CustomerBundle\Tests\Unit\Entity\Stub\WebsiteStub as Website;
+use Oro\Bundle\CustomerBundle\Tests\Unit\Entity\Stub\WebsiteStub;
 use Oro\Bundle\OrganizationBundle\Entity\Organization;
 use Oro\Bundle\UserBundle\Entity\User;
 use Oro\Bundle\UserBundle\Provider\DefaultUserProvider;
+use Oro\Bundle\WebsiteBundle\Entity\Website;
 use Oro\Bundle\WebsiteBundle\Manager\WebsiteManager;
 use Symfony\Component\PropertyAccess\PropertyAccessor;
 
 class GuestCustomerUserManagerTest extends \PHPUnit\Framework\TestCase
 {
-    /** @var GuestCustomerUserManager */
-    private $guestCustomerUserManager;
-
     /** @var WebsiteManager|\PHPUnit\Framework\MockObject\MockObject */
     private $websiteManager;
 
     /** @var CustomerUserManager|\PHPUnit\Framework\MockObject\MockObject */
     private $customerUserManager;
 
+    /** @var CustomerUserRelationsProvider|\PHPUnit\Framework\MockObject\MockObject */
+    private $relationsProvider;
+
     /** @var DefaultUserProvider|\PHPUnit\Framework\MockObject\MockObject */
     private $defaultUserProvider;
 
-    /** @var CustomerUserRelationsProvider|\PHPUnit\Framework\MockObject\MockObject */
-    private $relationsProvider;
+    /** @var GuestCustomerUserManager */
+    private $guestCustomerUserManager;
 
     /**
      * {@inheritdoc}
@@ -50,59 +52,85 @@ class GuestCustomerUserManagerTest extends \PHPUnit\Framework\TestCase
         );
     }
 
-    public function testGenerateGuestCustomerUser(): void
-    {
-        $this->customerUserManager
-            ->expects($this->once())
+    public function expectsGuestCustomerUserInitialization(
+        Website $website,
+        User $owner,
+        CustomerGroup $customerGroup
+    ): array {
+        $website->setName('Default Website');
+        $website->setOrganization(new Organization());
+        $website->setDefaultRole(new CustomerUserRole());
+
+        $owner->setFirstName('owner name');
+
+        $customerGroup->setName('Default Customer Group');
+        $customerGroup->setOwner($owner);
+
+        $this->customerUserManager->expects($this->once())
             ->method('generatePassword')
             ->with(10)
             ->willReturn('1234567890');
-
-        $owner = new User();
-        $owner->setFirstName('owner name');
-
-        $this->customerUserManager
-            ->expects($this->once())
+        $this->customerUserManager->expects($this->once())
             ->method('updatePassword');
 
-        $this->defaultUserProvider
-            ->expects($this->once())
+        $this->defaultUserProvider->expects($this->once())
             ->method('getDefaultUser')
             ->with('oro_customer', 'default_customer_owner')
             ->willReturn($owner);
 
-        $website = new Website();
-        $website->setName('Default Website');
-        $website->setOrganization(new Organization());
-        $website->setDefaultRole(new CustomerUserRole());
-        $this->websiteManager
-            ->expects($this->once())
+        $this->websiteManager->expects($this->once())
             ->method('getCurrentWebsite')
             ->willReturn($website);
 
-        $group = new CustomerGroup();
-        $group->setOwner(new User());
-        $group->setName('Default Group');
-        $this->relationsProvider
-            ->expects($this->once())
+        $this->relationsProvider->expects($this->once())
             ->method('getCustomerGroup')
-            ->willReturn($group);
+            ->willReturn($customerGroup);
 
-        $customerUser = $this->guestCustomerUserManager->generateGuestCustomerUser([
-            'email' => 'test@example.com',
+        return [
+            'email'      => 'test@example.com',
             'first_name' => 'First Name',
-            'last_name' => 'Last Name',
-        ]);
+            'last_name'  => 'Last Name'
+        ];
+    }
 
-        $this->assertEquals('test@example.com', $customerUser->getEmail());
-        $this->assertEquals('First Name', $customerUser->getFirstName());
-        $this->assertEquals('Last Name', $customerUser->getLastName());
+    public function assertGuestCustomerUserInitialization(
+        CustomerUser $customerUser,
+        array $properties,
+        Website $website,
+        User $owner,
+        CustomerGroup $customerGroup
+    ): void {
+        $this->assertEquals($properties['email'], $customerUser->getEmail());
+        $this->assertEquals($properties['first_name'], $customerUser->getFirstName());
+        $this->assertEquals($properties['last_name'], $customerUser->getLastName());
         $this->assertTrue($customerUser->isGuest());
         $this->assertFalse($customerUser->isConfirmed());
         $this->assertFalse($customerUser->isEnabled());
-        $this->assertEquals('Default Website', $customerUser->getWebsite()->getName());
-        $this->assertEquals('Default Group', $customerUser->getCustomer()->getGroup()->getName());
+        $this->assertSame($customerGroup, $customerUser->getCustomer()->getGroup());
         $this->assertEquals($owner->getFirstName(), $customerUser->getOwner()->getFirstName());
         $this->assertSame($customerUser->getRoles(), [$website->getDefaultRole()]);
+    }
+
+    public function testGenerateGuestCustomerUser(): void
+    {
+        $website = new WebsiteStub();
+        $owner = new User();
+        $customerGroup = new CustomerGroup();
+
+        $properties = $this->expectsGuestCustomerUserInitialization($website, $owner, $customerGroup);
+        $customerUser = $this->guestCustomerUserManager->generateGuestCustomerUser($properties);
+        $this->assertGuestCustomerUserInitialization($customerUser, $properties, $website, $owner, $customerGroup);
+    }
+
+    public function testInitializeGuestCustomerUser(): void
+    {
+        $website = new WebsiteStub();
+        $owner = new User();
+        $customerGroup = new CustomerGroup();
+
+        $properties = $this->expectsGuestCustomerUserInitialization($website, $owner, $customerGroup);
+        $customerUser = new CustomerUser();
+        $this->guestCustomerUserManager->initializeGuestCustomerUser($customerUser, $properties);
+        $this->assertGuestCustomerUserInitialization($customerUser, $properties, $website, $owner, $customerGroup);
     }
 }
