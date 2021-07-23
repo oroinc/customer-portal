3006e5444869e94dd8d2db7d1c65bc4a3f8ad8a6
Viacheslav Chekanovskiy <vchekanovskyi@oroinc.com>
2021-07-23 13:22:11 +0300

    BB-20767: "Esc" doesn't close the "Manage Filters" dropdown (#30328)
    * BB-20767: "Esc" doesn't close the "Manage Filters" dropdown
    - add `tabindex="-1"` to filter criteria
    - add focus manager force mode

--- src/Oro/Bundle/FrontendBundle/Resources/public/js/app/datafilter/frontend-collection-filters-manager.js
+++ src/Oro/Bundle/FrontendBundle/Resources/public/js/app/datafilter/frontend-collection-filters-manager.js
@@ -102,12 +102,13 @@ define(function(require, exports, module) {
          * @private
          */
         _createButtonReset: function() {
+            // Use link to keep focus even on disabled state
             return $(`
                 <div class="datagrid-manager__footer">
-                    <button class="btn btn--link btn--no-x-offset btn--no-y-offset"
-                        type="button" data-role="reset-filters">
+                    <a href="#" role="button" class="btn btn--link btn--no-x-offset btn--no-y-offset"
+                        data-role="reset-filters">
                         <span class="fa-refresh" aria-hidden="true"></span>${this.multiselectResetButtonLabel}
-                    </button>
+                    </a>
                 </div>
             `);
         },
