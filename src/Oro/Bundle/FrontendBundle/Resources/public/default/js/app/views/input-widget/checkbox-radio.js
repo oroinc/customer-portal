ed54c15ea4ba43b42dbda4a04036a3336ec94ba8
ValeriyYustunyk <ValeriyYustunyk@users.noreply.github.com>
2022-01-28 07:48:04 -0500

    BB-21095: Incorrect custom checkboxes rendering when using device switcher on demo (#31778)

--- src/Oro/Bundle/FrontendBundle/Resources/public/default/js/app/views/input-widget/checkbox-radio.js
+++ src/Oro/Bundle/FrontendBundle/Resources/public/default/js/app/views/input-widget/checkbox-radio.js
@@ -1,61 +1,27 @@
 define(function(require) {
     'use strict';
 
-    const $ = require('jquery');
-    const AbstractInputWidget = require('oroui/js/app/views/input-widget/abstract');
+    const FromInputCustomDesign = require('orofrontend/default/js/app/views/input-widget/from-input-custom-design');
 
-    const CheckboxRadioInputWidget = AbstractInputWidget.extend({
+    const CheckboxRadioInputWidget = FromInputCustomDesign.extend({
+        parentBaseCssClass: 'custom-radio',
+
+        parentCssClass: null,
+
+        parentAttrs: null,
+
+        iconBaseCssClass: 'custom-radio__text',
+
+        iconCssClass: null,
+
+        inputBaseCssClass: 'custom-radio__input',
+
+        inputCssClass: null,
+
         widgetFunction() {},
-
-        /**
-         * @inheritdoc
-         */
-        initializeWidget(options) {
-            CheckboxRadioInputWidget.__super__.initializeWidget.call(this, options);
-            this._applyDesignIfNeeded();
-        },
 
         findContainer() {
             return this.$el.closest('label');
-        },
-
-        _applyDesignIfNeeded() {
-            if (this.$el.parent(`.custom-radio`).length) {
-                return this;
-            }
-
-            const wrapEL = this.$el.parents('label').length ? '<span></span>' : '<label></label>';
-            this.$el.wrap($(wrapEL, {
-                'class': 'custom-radio'
-            })).after($('<span></span>', {
-                'class': 'custom-radio__text',
-                'aria-hidden': true
-            })).addClass('custom-radio__control')
-                .data('designApplied', true);
-
-            return this;
-        },
-
-        removeDesign() {
-            if (!this.$el.data('designApplied')) {
-                return;
-            }
-
-            this.$el
-                .unwrap()
-                .removeClass('custom-radio__control')
-                .removeData('designApplied')
-                .next()
-                .remove();
-        },
-
-        /**
-         * @inheritdoc
-         */
-        disposeWidget() {
-            this.removeDesign();
-
-            CheckboxRadioInputWidget.__super__.disposeWidget.call(this);
         }
     });
 
