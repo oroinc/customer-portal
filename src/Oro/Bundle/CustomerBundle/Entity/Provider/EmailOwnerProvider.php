07ffa7c9cef21afb0290a7ff09e473a6ae1d164c
Vova Soroka <vsoroka@oroinc.com>
2022-03-15 08:33:40 +0200

    CRM-9335: Public and private emails (#31652)
    Co-authored-by: Yurii Muratov 
    Co-authored-by: Kateryna Isakova

--- src/Oro/Bundle/CustomerBundle/Entity/Provider/EmailOwnerProvider.php
+++ src/Oro/Bundle/CustomerBundle/Entity/Provider/EmailOwnerProvider.php
@@ -3,11 +3,12 @@
 namespace Oro\Bundle\CustomerBundle\Entity\Provider;
 
 use Doctrine\ORM\EntityManager;
+use Oro\Bundle\BatchBundle\ORM\Query\BufferedQueryResultIterator;
 use Oro\Bundle\CustomerBundle\Entity\CustomerUser;
 use Oro\Bundle\EmailBundle\Entity\Provider\EmailOwnerProviderInterface;
 
 /**
- * Email owner provider for Customer User
+ * The email address owner provider for CustomerUser entity.
  */
 class EmailOwnerProvider implements EmailOwnerProviderInterface
 {
@@ -24,6 +25,45 @@ class EmailOwnerProvider implements EmailOwnerProviderInterface
      */
     public function findEmailOwner(EntityManager $em, $email)
     {
-        return $em->getRepository(CustomerUser::class)->findOneBy(['email' => $email]);
+        return $em->getRepository(CustomerUser::class)->findOneBy(['emailLowercase' => mb_strtolower($email)]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrganizations(EntityManager $em, $email)
+    {
+        $rows = $em->createQueryBuilder()
+            ->from(CustomerUser::class, 'cu')
+            ->select('IDENTITY(cu.organization) AS id')
+            ->where('cu.emailLowercase = :email')
+            ->setParameter('email', mb_strtolower($email))
+            ->getQuery()
+            ->getArrayResult();
+
+        $result = [];
+        foreach ($rows as $row) {
+            $result[] = (int)$row['id'];
+        }
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getEmails(EntityManager $em, $organizationId)
+    {
+        $qb = $em->createQueryBuilder()
+            ->from(CustomerUser::class, 'cu')
+            ->select('cu.email')
+            ->where('cu.organization = :organizationId')
+            ->setParameter('organizationId', $organizationId)
+            ->orderBy('cu.id');
+        $iterator = new BufferedQueryResultIterator($qb);
+        $iterator->setBufferSize(5000);
+        foreach ($iterator as $row) {
+            yield $row['email'];
+        }
     }
 }
