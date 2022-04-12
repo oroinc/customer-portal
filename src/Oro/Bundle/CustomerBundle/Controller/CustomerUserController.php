3eccea5dbb400968e297e31b73c66784b08a36f1
Chetverikov Yevhen <103036822+chetverikovyevhen@users.noreply.github.com>
2022-04-12 18:05:09 +0300

    BB-21230: "User Roles" checkbox disappears after validation error on Create Customer User page (#32375)
    - added handleRequest to the CustomerUserController::gerRolesAction;
    - added behat test

--- src/Oro/Bundle/CustomerBundle/Controller/CustomerUserController.php
+++ src/Oro/Bundle/CustomerBundle/Controller/CustomerUserController.php
@@ -18,6 +18,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
@@ -110,6 +111,7 @@ class CustomerUserController extends AbstractController
         $customerUser->setCustomer($customer);
 
         $form = $this->createForm(CustomerUserType::class, $customerUser);
+        $form->handleRequest($this->get(RequestStack::class)->getMainRequest());
 
         if (($error = $request->get('error', '')) && $form->has('userRoles')) {
             $form
@@ -210,6 +212,7 @@ class CustomerUserController extends AbstractController
                 TokenAccessorInterface::class,
                 CustomerUserManager::class,
                 LoggerInterface::class,
+                RequestStack::class,
             ]
         );
     }
--- src/Oro/Bundle/CustomerBundle/Tests/Behat/Features/create_customer_user.feature
+++ src/Oro/Bundle/CustomerBundle/Tests/Behat/Features/create_customer_user.feature
@@ -28,6 +28,16 @@ Feature: Create customer user
       | Email Address | This value is not a valid email address. |
     And should not see "Customer User has been saved" flash message
 
+  Scenario: Create Customer User with already existing email address
+    When I fill form with:
+      | Email Address     | test1e@example.org |
+      | Password          | Test1e@example.org |
+      | Confirm Password  | Test1e@example.org |
+    And save form
+    Then I should see validation errors:
+      | Email Address | This email is already used. |
+    And the "Buyer (Predefined)" checkbox should be checked
+
   Scenario: Create Customer User
     When I fill form with:
       | Email Address      | CustomerUser1@example.org |
@@ -38,3 +48,4 @@ Feature: Create customer user
     Then I should not see validation errors:
       | Email Address | This value is not a valid email address. |
     And should see "Customer User has been saved" flash message
+    And the "Buyer (Predefined)" checkbox should be checked
