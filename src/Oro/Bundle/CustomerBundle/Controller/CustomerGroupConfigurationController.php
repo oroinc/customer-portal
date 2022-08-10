59bd9cd3f65d64100c4d0d6d1301990b1e9d7924
Vova Soroka <vsoroka@oroinc.com>
2022-08-11 02:36:59 +0300

    BAP-21073: Marketplace Seller Organization Type Features Deactivation (#32850)
    Co-authored-by: Alexander Nezdoiminoga <alex@magecore.com>
    Co-authored-by: Yurii Muratov <ymuratov@oroinc.com>

--- src/Oro/Bundle/CustomerBundle/Controller/CustomerGroupConfigurationController.php
+++ src/Oro/Bundle/CustomerBundle/Controller/CustomerGroupConfigurationController.php
@@ -4,22 +4,31 @@ namespace Oro\Bundle\CustomerBundle\Controller;
 
 use Oro\Bundle\ConfigBundle\Config\ConfigManager;
 use Oro\Bundle\ConfigBundle\Form\Handler\ConfigHandler;
+use Oro\Bundle\ConfigBundle\Provider\AbstractProvider;
 use Oro\Bundle\CustomerBundle\Entity\CustomerGroup;
 use Oro\Bundle\CustomerBundle\Provider\CustomerGroupConfigurationFormProvider;
 use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
 use Oro\Bundle\SyncBundle\Content\DataUpdateTopicSender;
 use Oro\Bundle\SyncBundle\Content\TagGeneratorInterface;
+use Psr\Container\ContainerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Service\ServiceSubscriberInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
- * Handles configuration actions for the customer group config scope.
+ * The controller to handle the customer group configuration.
  */
-class CustomerGroupConfigurationController extends AbstractController
+class CustomerGroupConfigurationController implements ServiceSubscriberInterface
 {
+    private ContainerInterface $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
     /**
      * @Route(
      *      "/customerGroup/{id}/{activeGroup}/{activeSubGroup}",
@@ -36,33 +45,25 @@ class CustomerGroupConfigurationController extends AbstractController
         ?string $activeGroup = null,
         ?string $activeSubGroup = null
     ): array {
-        $provider = $this->get(CustomerGroupConfigurationFormProvider::class);
-        $manager = $this->get(ConfigManager::class);
+        $provider = $this->getConfigFormProvider();
+        $manager = $this->getConfigManager();
         $prevScopeId = $manager->getScopeId();
         $manager->setScopeId($entity->getId());
-
         [$activeGroup, $activeSubGroup] = $provider->chooseActiveGroups($activeGroup, $activeSubGroup);
-
-        $jsTree = $provider->getJsTree();
-        $form = false;
-
-        if ($activeSubGroup !== null) {
+        $form = null;
+        if (null !== $activeSubGroup) {
             $form = $provider->getForm($activeSubGroup, $manager);
-
-            if ($this->get(ConfigHandler::class)
-                ->setConfigManager($manager)
-                ->process($form, $request)
-            ) {
+            if ($this->getConfigHandler()->setConfigManager($manager)->process($form, $request)) {
                 $request->getSession()->getFlashBag()->add(
                     'success',
-                    $this->get(TranslatorInterface::class)->trans('oro.config.controller.config.saved.message')
+                    $this->getTranslator()->trans('oro.config.controller.config.saved.message')
                 );
 
-                $taggableData = ['name' => 'customer_group_configuration', 'params' => [$activeGroup, $activeSubGroup]];
-                $tagGenerator = $this->get(TagGeneratorInterface::class);
-                $dataUpdateTopicSender = $this->get(DataUpdateTopicSender::class);
-
-                $dataUpdateTopicSender->send($tagGenerator->generate($taggableData));
+                // outdate content tags, it's only special case for generation that are not covered by NavigationBundle
+                $this->getDataUpdateTopicSender()->send($this->getTagGenerator()->generate([
+                    'name'   => 'customer_group_configuration',
+                    'params' => [$activeGroup, $activeSubGroup]
+                ]));
 
                 // recreate form to drop values for fields with use_parent_scope_value
                 $form = $provider->getForm($activeSubGroup, $manager);
@@ -72,32 +73,59 @@ class CustomerGroupConfigurationController extends AbstractController
         $manager->setScopeId($prevScopeId);
 
         return [
-            'entity' => $entity,
-            'data' => $jsTree,
-            'form' => $form ? $form->createView() : null,
-            'activeGroup' => $activeGroup,
-            'activeSubGroup' => $activeSubGroup,
-            'scopeEntity' => $entity,
+            'entity'           => $entity,
+            'data'             => $provider->getJsTree(),
+            'form'             => $form?->createView(),
+            'activeGroup'      => $activeGroup,
+            'activeSubGroup'   => $activeSubGroup,
+            'scopeEntity'      => $entity,
             'scopeEntityClass' => CustomerGroup::class,
-            'scopeEntityId' => $entity->getId(),
+            'scopeEntityId'    => $entity->getId(),
         ];
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
     public static function getSubscribedServices(): array
     {
-        return array_merge(
-            parent::getSubscribedServices(),
-            [
-                ConfigManager::class,
-                TranslatorInterface::class,
-                TagGeneratorInterface::class,
-                ConfigHandler::class,
-                DataUpdateTopicSender::class,
-                CustomerGroupConfigurationFormProvider::class
-            ]
-        );
+        return [
+            CustomerGroupConfigurationFormProvider::class,
+            ConfigManager::class,
+            ConfigHandler::class,
+            TranslatorInterface::class,
+            TagGeneratorInterface::class,
+            DataUpdateTopicSender::class,
+        ];
+    }
+
+    private function getConfigFormProvider(): AbstractProvider
+    {
+        return $this->container->get(CustomerGroupConfigurationFormProvider::class);
+    }
+
+    private function getConfigManager(): ConfigManager
+    {
+        return $this->container->get(ConfigManager::class);
+    }
+
+    private function getConfigHandler(): ConfigHandler
+    {
+        return $this->container->get(ConfigHandler::class);
+    }
+
+    private function getTranslator(): TranslatorInterface
+    {
+        return $this->container->get(TranslatorInterface::class);
+    }
+
+    private function getTagGenerator(): TagGeneratorInterface
+    {
+        return $this->container->get(TagGeneratorInterface::class);
+    }
+
+    private function getDataUpdateTopicSender(): DataUpdateTopicSender
+    {
+        return $this->container->get(DataUpdateTopicSender::class);
     }
 }
