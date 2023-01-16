2fb80096b7d06a522f7012bc0418d6a7ccf93d3b
Hryhorii Hrebiniuk <hhrebiniuk@oroinc.com>
2023-01-16 21:52:24 +0200

    BB-22041: Action button drop-down is only partially visible on back-office product view page on mobile (#34654)

--- src/Oro/Bundle/FrontendBundle/Resources/public/blank/js/app/service/modals-scroll-locker.js
+++ src/Oro/Bundle/FrontendBundle/Resources/public/blank/js/app/service/modals-scroll-locker.js
@@ -1,41 +1,6 @@
-import $ from 'jquery';
 import Backbone from 'backbone';
 import mediator from 'oroui/js/mediator';
-
-const modals = {};
-let isScrollLocked = false;
-
-const scrollUpdate = () => {
-    if (isScrollLocked === Object.keys(modals).length > 0) {
-        return; // nothing to toggle
-    }
-
-    const $body = $('body');
-    isScrollLocked = !isScrollLocked;
-    if (isScrollLocked) {
-        $body.css({
-            position: 'fixed',
-            top: `-${window.scrollY}px`
-        });
-    } else {
-        const scrollY = document.body.style.top;
-        $body.css({
-            position: '',
-            top: ''
-        });
-        window.scrollTo(0, parseInt(scrollY || '0') * -1);
-    }
-};
-
-const addModal = cid => {
-    modals[cid] = true;
-    scrollUpdate();
-};
-
-const removeModal = cid => {
-    delete modals[cid];
-    scrollUpdate();
-};
+import scrollLocker from 'oroui/js/app/services/body-scroll-locker';
 
 export default () => {
     const observer = Object.create(Backbone.Events);
@@ -44,16 +9,16 @@ export default () => {
         // widget dialogs
         'widget_dialog:open'(dialog) {
             if (dialog.widget.dialog('instance').options.modal) {
-                addModal(dialog.cid);
-                observer.listenToOnce(dialog, 'dispose', removeModal.bind(void 0, dialog.cid));
+                scrollLocker.addLocker(dialog.cid);
+                observer.listenToOnce(dialog, 'dispose', () => scrollLocker.removeLocker(dialog.cid));
             }
         },
         'widget_dialog:close'(dialog) {
-            removeModal(dialog.cid);
+            scrollLocker.removeLocker(dialog.cid);
             observer.stopListening(dialog);
         },
         // modals
-        'modal:open': modal => addModal(modal.cid),
-        'modal:close': modal => removeModal(modal.cid)
+        'modal:open': modal => scrollLocker.addLocker(modal.cid),
+        'modal:close': modal => scrollLocker.removeLocker(modal.cid)
     });
 };
