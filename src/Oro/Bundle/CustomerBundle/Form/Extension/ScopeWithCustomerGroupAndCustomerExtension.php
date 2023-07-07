dcf98cfd3f30b4ec996f0922bcbf5fa857456e36
oro-panasiukr <39378223+oro-panasiukr@users.noreply.github.com>
2023-07-07 11:37:12 +0300

    BB-22151: Non-default content variant of content block can be saved  (#35946)
    * BB-22151: Non-default content variant of content block can be saved without restrictions (#35692)

---
+++ src/Oro/Bundle/CustomerBundle/Form/Extension/ScopeWithCustomerGroupAndCustomerExtension.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Oro\Bundle\CustomerBundle\Form\Extension;
+
+use Oro\Bundle\CustomerBundle\Validator\Constraints\ScopeWithCustomerGroupAndCustomer;
+use Oro\Bundle\FormBundle\Utils\FormUtils;
+use Oro\Bundle\ScopeBundle\Form\Type\ScopeCollectionType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Event\PreSubmitEvent;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+
+/**
+ * Adds additional validation for scopes collection.
+ * Сan not use scopes at the same time as custom and customer group.
+ */
+class ScopeWithCustomerGroupAndCustomerExtension extends AbstractTypeExtension
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (PreSubmitEvent $event) {
+            $form = $event->getForm();
+            $constraints = ['constraints' => [new ScopeWithCustomerGroupAndCustomer()]];
+            foreach ($form as $index => $field) {
+                FormUtils::mergeFieldOptionsRecursive($form, $index, $constraints);
+            }
+        });
+    }
+
+    public static function getExtendedTypes(): iterable
+    {
+        return [ScopeCollectionType::class];
+    }
+}
