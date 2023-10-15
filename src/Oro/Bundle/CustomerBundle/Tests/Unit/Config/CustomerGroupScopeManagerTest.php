b110994a45459eff9fded0ad94802ac8be320bc0
Vova Soroka <vsoroka@oroinc.com>
2023-10-16 01:19:44 +0300

    BAP-22271: Add strict types to config scope managers (#36652)

--- src/Oro/Bundle/CustomerBundle/Tests/Unit/Config/CustomerGroupScopeManagerTest.php
+++ src/Oro/Bundle/CustomerBundle/Tests/Unit/Config/CustomerGroupScopeManagerTest.php
@@ -2,160 +2,40 @@
 
 namespace Oro\Bundle\CustomerBundle\Tests\Unit\Config;
 
-use Doctrine\Persistence\ManagerRegistry;
-use Oro\Bundle\ConfigBundle\Config\ConfigBag;
 use Oro\Bundle\ConfigBundle\Tests\Unit\Config\AbstractScopeManagerTestCase;
 use Oro\Bundle\CustomerBundle\Config\CustomerGroupScopeManager;
 use Oro\Bundle\CustomerBundle\Entity\Customer;
 use Oro\Bundle\CustomerBundle\Entity\CustomerGroup;
 use Oro\Bundle\CustomerBundle\Entity\CustomerUser;
 use Oro\Bundle\OrganizationBundle\Entity\Organization;
-use Symfony\Component\EventDispatcher\EventDispatcher;
+use Oro\Component\Testing\ReflectionUtil;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Contracts\Cache\CacheInterface;
 
 class CustomerGroupScopeManagerTest extends AbstractScopeManagerTestCase
 {
     /** @var TokenStorageInterface|\PHPUnit\Framework\MockObject\MockObject */
-    private TokenStorageInterface $tokenStorage;
+    private $tokenStorage;
 
     protected function setUp(): void
     {
-        parent::setUp();
-
         $this->tokenStorage = $this->createMock(TokenStorageInterface::class);
-
-        $this->manager->setTokenStorage($this->tokenStorage);
-    }
-
-    public function testInitializeScopeId(): void
-    {
-        $token = $this->createMock(TokenInterface::class);
-
-        $customer = $this->getEntity(Customer::class, [
-            'group' => $this->getScopedEntity()
-        ]);
-        $token->expects($this->once())
-            ->method('getUser')
-            ->willReturn($this->getEntity(CustomerUser::class, ['id' => 123, 'customer' => $customer]));
-
-        $this->tokenStorage->expects($this->once())
-            ->method('getToken')
-            ->willReturn($token);
-
-        $this->assertEquals(456, $this->manager->getScopeId());
-    }
-
-    public function testInitializeScopeIdForUserWithEmptyCustomer(): void
-    {
-        $token = $this->createMock(TokenInterface::class);
-
-        $token->expects($this->once())
-            ->method('getUser')
-            ->willReturn(new CustomerUser());
-
-        $this->tokenStorage->expects($this->once())
-            ->method('getToken')
-            ->willReturn($token);
-
-        $this->assertEquals(0, $this->manager->getScopeId());
-    }
-
-    public function testInitializeScopeIdForUserWithEmptyCustomerGroup(): void
-    {
-        $token = $this->createMock(TokenInterface::class);
-
-        $customerUser = $this->getEntity(CustomerUser::class, [
-            'customer' => new Customer()
-        ]);
-
-        $token->expects($this->once())
-            ->method('getUser')
-            ->willReturn($customerUser);
-
-        $this->tokenStorage->expects($this->once())
-            ->method('getToken')
-            ->willReturn($token);
-
-        $this->assertEquals(0, $this->manager->getScopeId());
-    }
-
-    public function testInitializeScopeIdForUserWithEmptyCustomerGroupIdentity(): void
-    {
-        $token = $this->createMock(TokenInterface::class);
-
-        $customer = $this->getEntity(Customer::class, [
-            'group' => new CustomerGroup()
-        ]);
-        $customerUser = $this->getEntity(CustomerUser::class, [
-            'id' => 132,
-            'customer' => $customer
-        ]);
-
-        $token->expects($this->once())
-            ->method('getUser')
-            ->willReturn($customerUser);
-
-        $this->tokenStorage->expects($this->once())
-            ->method('getToken')
-            ->willReturn($token);
-
-        $this->assertEquals(0, $this->manager->getScopeId());
-    }
-
-    public function testInitializeScopeIdForUnsupportedUserObject(): void
-    {
-        $token = $this->createMock(TokenInterface::class);
-        $token->expects($this->once())
-            ->method('getUser')
-            ->willReturn(new Organization());
-
-        $this->tokenStorage->expects($this->once())
-            ->method('getToken')
-            ->willReturn($token);
-
-        $this->assertEquals(0, $this->manager->getScopeId());
-    }
-
-    public function testInitializeScopeIdNoToken(): void
-    {
-        $this->tokenStorage->expects($this->once())
-            ->method('getToken')
-            ->willReturn(null);
-
-        $this->assertEquals(0, $this->manager->getScopeId());
-    }
-
-    public function testSetScopeId(): void
-    {
-        $this->tokenStorage->expects($this->never())
-            ->method('getToken');
-
-        $this->manager->setScopeId(789);
-
-        $this->assertEquals(789, $this->manager->getScopeId());
+        parent::setUp();
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
-    protected function createManager(
-        ManagerRegistry $doctrine,
-        CacheInterface  $cache,
-        EventDispatcher $eventDispatcher,
-        ConfigBag       $configBag
-    ): CustomerGroupScopeManager {
-        return new CustomerGroupScopeManager(
-            $doctrine,
-            $cache,
-            $eventDispatcher,
-            $configBag,
-        );
+    protected function createManager(): CustomerGroupScopeManager
+    {
+        $manager = new CustomerGroupScopeManager($this->doctrine, $this->cache, $this->dispatcher, $this->configBag);
+        $manager->setTokenStorage($this->tokenStorage);
+
+        return $manager;
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
     protected function getScopedEntityName(): string
     {
@@ -163,10 +43,118 @@ class CustomerGroupScopeManagerTest extends AbstractScopeManagerTestCase
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
     protected function getScopedEntity(): CustomerGroup
     {
-        return $this->getEntity(CustomerGroup::class, ['id' => 456]);
+        $entity = new CustomerGroup();
+        ReflectionUtil::setId($entity, 456);
+
+        return $entity;
+    }
+
+    private function getCustomerUser(): CustomerUser
+    {
+        $customerUser = new CustomerUser();
+        ReflectionUtil::setId($customerUser, 123);
+
+        return $customerUser;
+    }
+
+    public function testInitializeScopeId(): void
+    {
+        $customer = new Customer();
+        $customer->setGroup($this->getScopedEntity());
+
+        $customerUser = $this->getCustomerUser();
+        $customerUser->setCustomer($customer);
+
+        $token = $this->createMock(TokenInterface::class);
+        $token->expects(self::once())
+            ->method('getUser')
+            ->willReturn($customerUser);
+        $this->tokenStorage->expects(self::once())
+            ->method('getToken')
+            ->willReturn($token);
+
+        self::assertSame(456, $this->manager->getScopeId());
+    }
+
+    public function testInitializeScopeIdForUserWithEmptyCustomer(): void
+    {
+        $token = $this->createMock(TokenInterface::class);
+        $token->expects(self::once())
+            ->method('getUser')
+            ->willReturn($this->getCustomerUser());
+        $this->tokenStorage->expects(self::once())
+            ->method('getToken')
+            ->willReturn($token);
+
+        self::assertSame(0, $this->manager->getScopeId());
+    }
+
+    public function testInitializeScopeIdForUserWithEmptyCustomerGroup(): void
+    {
+        $customerUser = $this->getCustomerUser();
+        $customerUser->setCustomer(new Customer());
+
+        $token = $this->createMock(TokenInterface::class);
+        $token->expects(self::once())
+            ->method('getUser')
+            ->willReturn($customerUser);
+        $this->tokenStorage->expects(self::once())
+            ->method('getToken')
+            ->willReturn($token);
+
+        self::assertSame(0, $this->manager->getScopeId());
+    }
+
+    public function testInitializeScopeIdForUnsupportedUserObject(): void
+    {
+        $token = $this->createMock(TokenInterface::class);
+        $token->expects(self::once())
+            ->method('getUser')
+            ->willReturn(new Organization());
+        $this->tokenStorage->expects(self::once())
+            ->method('getToken')
+            ->willReturn($token);
+
+        self::assertSame(0, $this->manager->getScopeId());
+    }
+
+    public function testInitializeScopeIdNoToken(): void
+    {
+        $this->tokenStorage->expects(self::once())
+            ->method('getToken')
+            ->willReturn(null);
+
+        self::assertSame(0, $this->manager->getScopeId());
+    }
+
+    public function testGetScopeIdFromEntityForCustomerGroupWithEmptyId(): void
+    {
+        self::assertSame(0, $this->manager->getScopeIdFromEntity(new CustomerGroup()));
+    }
+
+    public function testGetScopeIdFromEntityForCustomer(): void
+    {
+        $customerGroup = $this->getScopedEntity();
+        $customer = new Customer();
+        $customer->setGroup($customerGroup);
+
+        self::assertSame($customerGroup->getId(), $this->manager->getScopeIdFromEntity($customer));
+    }
+
+    public function testGetScopeIdFromEntityForCustomerAssociatedToCustomerGroupWithEmptyId(): void
+    {
+        $customer = new Customer();
+        $customer->setGroup(new CustomerGroup());
+
+        self::assertSame(0, $this->manager->getScopeIdFromEntity($customer));
+    }
+
+    public function testGetScopeIdFromEntityForCustomerWithoutCustomerGroup(): void
+    {
+        self::assertNull($this->manager->getScopeIdFromEntity(new Customer()));
     }
 }
