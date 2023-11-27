c16b8c6e184eec7c44d02a085165cfd6b2166e3c
oro-panasiuk <rpanasiuk@oroinc.com>
2023-11-27 19:51:41 +0200

    BB-23057: Pinned entity creation form shows wrong data

---
+++ src/Oro/Bundle/CustomerBundle/Migrations/Schema/v_1_32/UpdatePageStateHash.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Oro\Bundle\CustomerBundle\Migrations\Schema\v_1_32;
+
+use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+use Oro\Bundle\MigrationBundle\Migration\SqlMigrationQuery;
+
+/**
+ * Updates the hashes of pinned pages, since the hash must contain the customer user ID.
+ */
+class UpdatePageStateHash implements Migration
+{
+    public function up(Schema $schema, QueryBag $queries): void
+    {
+        $queries->addPreQuery(
+            new SqlMigrationQuery(
+                'UPDATE oro_cus_pagestate SET page_hash = MD5(CONCAT(page_id, \'_\', customer_user_id))'
+            )
+        );
+    }
+}
