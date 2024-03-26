15c2ad9afe3427b0e38774cf66bbd1cb913bf3e8
Roman Kukla <kukla.roman@gmail.com>
2024-03-26 18:55:41 +0200

    Revert "BB-23843: Not all lineitems are shown in storefront order print view …" (#37893)

--- src/Oro/Bundle/FrontendBundle/Resources/public/default/js/widgets/print-page-widget.js
+++ src/Oro/Bundle/FrontendBundle/Resources/public/default/js/widgets/print-page-widget.js
@@ -1,15 +1,10 @@
 define(function(require) {
     'use strict';
 
-    const routing = require('routing');
     const $ = require('jquery');
-    const LoadingMask = require('oroui/js/app/views/loading-mask-view');
     require('jquery-ui/widget');
 
     $.widget('oroui.printPageWidget', {
-
-        loadingMask: new LoadingMask({container: $('body')}),
-
         _create: function() {
             this._super();
             this.$el = this.element;
@@ -27,34 +22,12 @@ define(function(require) {
 
         _windowPrint: function(event) {
             const $trigger = $(event.currentTarget);
+
             if ($trigger.attr('href')) {
                 event.preventDefault();
             }
 
-            const printFrame = document.createElement('iframe');
-            printFrame.src = routing.generate('oro_order_frontend_print', {id: this.$el.attr('value')});
-            printFrame.onload = function(self) {
-                const closePrint = () => {
-                    document.body.removeChild(this);
-                    self._hideMask();
-                };
-
-                // If close the print window without printing.
-                this.contentWindow.onbeforeunload = closePrint;
-                // After printing the order page.
-                this.contentWindow.onafterprint = closePrint;
-            }.bind(printFrame, this);
-
-            this._showMask();
-            document.body.appendChild(printFrame);
-        },
-
-        _showMask: function() {
-            this.loadingMask.show();
-        },
-
-        _hideMask: function() {
-            this.loadingMask.hide();
+            window.print();
         }
     });
 
