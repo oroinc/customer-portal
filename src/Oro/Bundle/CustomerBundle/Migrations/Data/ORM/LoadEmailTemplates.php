729946f721515bffa2ec2cde0a7ca099523cb020
Maksym Maksymenko <fubolg.ua@gmail.com>
2024-07-19 19:08:26 +0300

    BB-21035: Email Template Inheritance (part 2) (#38557)
    * BB-23807 - [Backend] Finishing  email templates with products
    
    - updated order_confirmation_email.html.twig added fynamic fields to email template
    - updated OrderExtension added oro_order_get_shipping_trackings twig function
    - updated TwigSandboxConfigurationPass added oro_order_get_shipping_trackings twig function to email templates rendering sandbox
    
    * BB-23807 - [Backend] Finishing  email templates with products
    
    - updated ConfigurableProductProvider getParentProductFromLineItem method now retrieves the parent product from the first ParentVariantLink if getParentProduct returned null
    - updated LineItemsExtension variables seller_name, variant_fields_values for Order Line Item and product_sku, variant_fields_values for Kit Item Line Items
    now available in email templates
    - updated order_confirmation_email.html.twig The email template is now more dynamic
    - updated OrderProductKitItemLineItem now implements ProductHolderInterface
    - updated LineItemsExtension fixed variant_fields_values field for configurable line item
    - updated ConfigurableProductProvider reverted getParentProductFromLineItem method behavior
    - updated request_create_confirmation.html.twig dynamic data added
    
    * BB-24050 - [Backend] Redesign RFQ and Quote emails
    
    - updated RequestProductsExtension added productSku for kitItemLine and sellerName for line item
    - updated request_create_confirmation.html.twig all dynamic data added
    - added QuoteProductsExtension provides quote_products twig function
    - updated request_create_confirmation.html.twig all dynamic data added
    - updated quote_email_link.html.twig all dynamic data added
    - updated TwigSandboxConfigurationPass added 'quote_products' twig function for the email templates rendering sandbox
    
    * BB-23945 Broken Email template on sending via MORE ACTIONS-button (#38563)
    
    - removed extra HTML comments from base template
    - added new md5 hashes for email template migrations
    - fixed impossible to update base_storefront email template
    
    * BB-23917: Behat tests (#38544)
    
    * BB-23945: Broken Email template on sending via MORE ACTIONS-button (#38698)
    
    - removed extra <!--HTML comments--> from base template base_storefront.html.twig
    - updated EmailType style tags allowed for WYSIWYG
    - fixed email template styles
    - added new md5 hashes for email template migrations
    - updated TinyMCE configuration;
    - updated EmailModelSender to dynamically add doctype;
    - fixed product name for free form product

--- src/Oro/Bundle/CustomerBundle/Migrations/Data/ORM/LoadEmailTemplates.php
+++ src/Oro/Bundle/CustomerBundle/Migrations/Data/ORM/LoadEmailTemplates.php
@@ -30,23 +30,33 @@ class LoadEmailTemplates extends AbstractHashEmailMigration implements Versioned
                 'd970bd18538742a4702e70df6f14444d', // 1.0
                 '6f2554689920e2d47ac6ea044fdd8e43', // 1.2
                 '61e82b3d8c7180e362738a98e266f037', // 1.3
+                '54a5dee6405f3d9e1660fe0f3dcb21c1', // 1.5
+                '54a5dee6405f3d9e1660fe0f3dcb21c1', // 1.6
             ],
             'customer_user_welcome_email_registered_by_admin' => [
                 'e583b8b7cdea31f8f0ce0a4000b956b9', // 1.1
                 'e2a34aa359ce8d958abc7c3eddd7bc93', // 1.3
+                'a36f8bfd13af225f6fb5a1c79397bb4f', // 1.5
+                'a36f8bfd13af225f6fb5a1c79397bb4f', // 1.6
             ],
             'customer_user_confirmation_email' => [
                 '47e012b40cec188ad88dfb7e3379446d', // 1.1
                 'e7d7fe65e8b2778b333b5b8f6220ed55', // 1.3
+                'cbe6cc8b395a5aa7d0642220b1cacfd9', // 1.5
+                'cbe6cc8b395a5aa7d0642220b1cacfd9', // 1.6
             ],
             'customer_user_reset_password' => [
                 '4c987be76cdffc3ade87c9fca27a60be', // 1.1
                 '02c65afdfb3e2c61c0c31cd2ff096d0d', // 1.3
                 '2d072b726d9f03c3fb0b85357e6c0fca', // 1.4
+                'a03885915c75cb0ead5b4e8dc21c457e', // 1.5
+                'a03885915c75cb0ead5b4e8dc21c457e', // 1.6
             ],
             'customer_user_force_reset_password' => [
                 'beb25a213aa466f95ae48d710478fa13', // 1.3
-                'd9c8afadce0cee68730210c3d50b0d9e'  // 1.4
+                'd9c8afadce0cee68730210c3d50b0d9e', // 1.4
+                '52324c35721f05c73b6c5509633c7908', // 1.5
+                '52324c35721f05c73b6c5509633c7908', // 1.6
             ],
         ];
     }
