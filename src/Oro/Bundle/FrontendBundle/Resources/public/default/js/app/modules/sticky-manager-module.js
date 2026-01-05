bbebbdbfbc777951f8e282e1e995abfefccbd2c2
Alex Yegerev <1755578+lexxcode@users.noreply.github.com>
2026-01-05 18:36:21 +0400

    BB-25858: Improve Google INP metrics (#42687)
    * BB-25858: Improve Google INP metrics
    - Added Scheduler
    - Split big task to few small tasks
    - Add async init of InputWidgets
    - Add scheduler for Application
    - Optimize Style Recalculation
    - Avoid Force Reflow
    - Swap $.Deffered to vanilla Promise for Component Manager
    - Integrate Scheduler in to Component Manager
    - Fix tests

--- src/Oro/Bundle/FrontendBundle/Resources/public/default/js/app/modules/sticky-manager-module.js
+++ src/Oro/Bundle/FrontendBundle/Resources/public/default/js/app/modules/sticky-manager-module.js
@@ -4,13 +4,15 @@ import 'sticky-element';
 const stickyName = 'sticky';
 
 $(document)
-    .on('initLayout content:changed', ({target}) => {
+    .on('initLayout content:changed', async ({target}) => {
+        await window.scheduler.yield();
         $(target).find(`[data-${stickyName}]`).each((_, element) => {
             const data = $(element).data(stickyName) ?? {};
             $(element).stickyElement(typeof data === 'object' ? data : {});
         });
     })
-    .on('disposeLayout content:remove', ({target}) => {
+    .on('disposeLayout content:remove', async ({target}) => {
+        await window.scheduler.yield();
         $(target).find(`[data-${stickyName}]`).each((_, element) => {
             if ($(element).data(`oro.stickyElement`)) {
                 $(element).stickyElement('dispose');
