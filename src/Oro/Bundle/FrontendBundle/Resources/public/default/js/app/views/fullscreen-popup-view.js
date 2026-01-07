dc80f06a18657fb79d37a43074e245b2f1e7a8ff
Alexander Kovalenko <okovalenko@oroinc.com>
2026-01-07 17:17:27 +0200

    BB-26617: First menu item has visible focused styles when opening main menu in Safari (#43537)
    - First focused item after menu is opened now doesn't have a focused state.
    - Fixed focused state ui for standalone menu.
    - Removed unused logic related to focus-via-arrows-keys css class which is not used anymore.
    - Fixed issue when first item is not focused when menu is opened via keyboard.

--- src/Oro/Bundle/FrontendBundle/Resources/public/default/js/app/views/fullscreen-popup-view.js
+++ src/Oro/Bundle/FrontendBundle/Resources/public/default/js/app/views/fullscreen-popup-view.js
@@ -8,11 +8,10 @@ define(function(require) {
     const loadModules = require('oroui/js/app/services/load-modules');
     const mediator = require('oroui/js/mediator');
     const scrollHelper = require('oroui/js/tools/scroll-helper');
+    const KEY_CODES = require('oroui/js/tools/keyboard-key-codes').default;
     const _ = require('underscore');
     const $ = require('jquery');
     const manageFocus = require('oroui/js/tools/manage-focus').default;
-
-    const ESCAPE_KEYCODE = 27;
 
     const FullscreenPopupView = BaseView.extend({
         /**
@@ -79,7 +78,9 @@ define(function(require) {
         previousClass: null,
 
         events: {
-            click: 'show'
+            click: 'show',
+            keydown: 'handleKeyDown',
+            mousedown: 'handleMouseDown'
         },
 
         /**
@@ -111,6 +112,12 @@ define(function(require) {
         toggleBtnActiveClassName: 'fullscreen-popup__opened',
 
         /**
+         * Track if the last interaction was via mouse
+         * @property {boolean}
+         */
+        isMouseInteraction: true,
+
+        /**
          * @inheritdoc
          */
         constructor: function FullscreenPopupView(options) {
@@ -123,7 +130,27 @@ define(function(require) {
         initialize: function(options) {
             this.initLayoutOptions = options.initLayoutOptions || {};
             _.each(this.sections, this._initializeSection.bind(this, options));
+
+            this.onMouseDown = this.handleMouseDown.bind(this);
+            this.onKeyDown = this.handleKeyDown.bind(this);
+            document.addEventListener('mousedown', this.onMouseDown, true);
+            document.addEventListener('keydown', this.onKeyDown, true);
+
             return FullscreenPopupView.__super__.initialize.call(this, options);
+        },
+
+        handleMouseDown: function() {
+            this.isMouseInteraction = true;
+        },
+
+        handleKeyDown: function(event) {
+            if (this.isKeyboardInteraction(event.keyCode)) {
+                this.isMouseInteraction = false;
+            }
+        },
+
+        isKeyboardInteraction: function(keyCode) {
+            return FullscreenPopupView.KEYBOARD_INTERACTION_KEYS.includes(keyCode);
         },
 
         /**
@@ -133,6 +160,7 @@ define(function(require) {
             if (this.disposed) {
                 return;
             }
+
             this.remove();
             FullscreenPopupView.__super__.dispose.call(this);
         },
@@ -140,6 +168,10 @@ define(function(require) {
         show: function() {
             this.close();
             this.$popup = $(this.getTemplateFunction()(this.getTemplateData()));
+
+            if (!this.isMouseInteraction) {
+                this.$popup.attr('data-focus-visible', '');
+            }
 
             this.$popup.appendTo($('body'));
 
@@ -343,7 +375,7 @@ define(function(require) {
                 .on('click', '[data-role="close"]', this.close.bind(this))
                 .on('touchstart', '[data-scroll="true"]', scrollHelper.removeIOSRubberEffect.bind(this))
                 .on('keydown', e => {
-                    if (e.keyCode === ESCAPE_KEYCODE) {
+                    if (e.keyCode === KEY_CODES.ESCAPE) {
                         e.stopPropagation();
                         this.close();
                         this.$el.trigger('focus');
@@ -385,6 +417,16 @@ define(function(require) {
         getFocusTabbableElement() {
             return this.$popup;
         }
+    }, {
+        KEYBOARD_INTERACTION_KEYS: [
+            KEY_CODES.ENTER,
+            KEY_CODES.SPACE,
+            KEY_CODES.TAB,
+            KEY_CODES.ARROW_LEFT,
+            KEY_CODES.ARROW_UP,
+            KEY_CODES.ARROW_RIGHT,
+            KEY_CODES.ARROW_DOWN
+        ]
     });
 
     return FullscreenPopupView;
