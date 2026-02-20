8b1e777164dbfa7e183ae9345d8fa199e72e95c3
vsheverov <120404762+vsheverov@users.noreply.github.com>
2026-02-20 19:09:27 +0100

    BB-26146: Missing validation for customer and customer user relation in API (#44016)
    - added `ValidateInvoiceCustomerOwner` to ensure that customer users are correctly validated against their associated customers when creating or updating invoices.
    - updated `AddCustomerOwnerValidator` to include checks for frontend ownership and added necessary dependencies.
    - updated `CustomerOwnerValidator` to improve ownership validation logic and handle frontend ownership metadata.

--- src/Oro/Bundle/CustomerBundle/Validator/Constraints/CustomerOwnerValidator.php
+++ src/Oro/Bundle/CustomerBundle/Validator/Constraints/CustomerOwnerValidator.php
@@ -2,8 +2,14 @@
 
 namespace Oro\Bundle\CustomerBundle\Validator\Constraints;
 
+use Doctrine\Common\Util\ClassUtils;
+use Oro\Bundle\CustomerBundle\Entity\Customer;
 use Oro\Bundle\CustomerBundle\Entity\CustomerOwnerAwareInterface;
+use Oro\Bundle\CustomerBundle\Entity\CustomerUser;
+use Oro\Bundle\CustomerBundle\Owner\Metadata\FrontendOwnershipMetadata;
+use Oro\Bundle\CustomerBundle\Owner\Metadata\FrontendOwnershipMetadataProvider;
 use Oro\Bundle\SecurityBundle\Acl\BasicPermission;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
@@ -16,14 +22,12 @@ use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 class CustomerOwnerValidator extends ConstraintValidator
 {
     public function __construct(
-        private AuthorizationCheckerInterface $authorizationChecker
+        private AuthorizationCheckerInterface $authorizationChecker,
+        private FrontendOwnershipMetadataProvider $frontendOwnershipMetadataProvider,
+        private PropertyAccessorInterface $propertyAccessor
     ) {
     }
 
-    /**
-     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
-     * @SuppressWarnings(PHPMD.NPathComplexity)
-     */
     #[\Override]
     public function validate($value, Constraint $constraint): void
     {
@@ -35,17 +39,33 @@ class CustomerOwnerValidator extends ConstraintValidator
             return;
         }
 
-        if (!$value instanceof CustomerOwnerAwareInterface) {
-            throw new UnexpectedTypeException($value, CustomerOwnerAwareInterface::class);
-        }
+        if ($value instanceof CustomerOwnerAwareInterface) {
+            $customer = $value->getCustomer();
+            if (null === $customer) {
+                return;
+            }
+            $customerUser = $value->getCustomerUser();
+            $this->validateOwnership($customer, $customerUser, $constraint);
 
-        $customer = $value->getCustomer();
-        if (null === $customer) {
             return;
         }
 
-        $customerUser = $value->getCustomerUser();
-        if (null === $customerUser) {
+        $metadata = $this->getFrontendOwnershipMetadata(ClassUtils::getClass($value));
+        if ($metadata === null) {
+            return;
+        }
+
+        $customer = $this->propertyAccessor->getValue($value, $metadata->getCustomerFieldName());
+        $customerUser = $this->propertyAccessor->getValue($value, $metadata->getOwnerFieldName());
+        $this->validateOwnership($customer, $customerUser, $constraint);
+    }
+
+    private function validateOwnership(
+        ?Customer $customer,
+        ?CustomerUser $customerUser,
+        CustomerOwner $constraint
+    ): void {
+        if (null === $customer || null === $customerUser) {
             return;
         }
 
@@ -61,5 +81,21 @@ class CustomerOwnerValidator extends ConstraintValidator
         ) {
             $this->context->addViolation($constraint->message);
         }
+    }
+
+    private function getFrontendOwnershipMetadata(string $entityClass): ?FrontendOwnershipMetadata
+    {
+        $metadata = $this->frontendOwnershipMetadataProvider->getMetadata($entityClass);
+
+        if (
+            $metadata instanceof FrontendOwnershipMetadata
+            && $metadata->hasOwner()
+            && $metadata->isUserOwned()
+            && $metadata->getCustomerFieldName()
+        ) {
+            return $metadata;
+        }
+
+        return null;
     }
 }
