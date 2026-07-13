2cefde738c9d46e6998d9ff63c42db7dac3f7584
Mykhailo Kotliar <mkotliar@oroinc.com>
2026-07-13 20:43:20 +0300

    BB-27517: "External ID" field should be not visible by default (7.0) (#45870)

---
+++ src/Oro/Bundle/CustomerBundle/Migrations/Schema/v7_0_3_1/HideCustomerAndCustomerUserExternalIdField.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Oro\Bundle\CustomerBundle\Migrations\Schema\v7_0_3_1;
+
+use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\CustomerBundle\Entity\Customer;
+use Oro\Bundle\CustomerBundle\Entity\CustomerUser;
+use Oro\Bundle\EntityBundle\EntityConfig\DatagridScope;
+use Oro\Bundle\EntityConfigBundle\Migration\UpdateEntityConfigFieldValueQuery;
+use Oro\Bundle\EntityExtendBundle\Migration\ExtendOptionsManagerAwareInterface;
+use Oro\Bundle\EntityExtendBundle\Migration\ExtendOptionsManagerAwareTrait;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+/**
+ * Fully hides externalId field of Customer and CustomerUser entities from grid, filters, view and edit form.
+ */
+class HideCustomerAndCustomerUserExternalIdField implements Migration, ExtendOptionsManagerAwareInterface
+{
+    use ExtendOptionsManagerAwareTrait;
+
+    #[\Override]
+    public function up(Schema $schema, QueryBag $queries): void
+    {
+        $this->hideCustomerExternalId($queries);
+        $this->hideCustomerUserExternalId($queries);
+    }
+
+    private function hideCustomerExternalId(QueryBag $queries): void
+    {
+        // Works in case when the affected field does not yet exist.
+        $this->extendOptionsManager->mergeColumnOptions(
+            'oro_customer',
+            'external_id',
+            [
+                'datagrid' => ['is_visible' => DatagridScope::IS_VISIBLE_FALSE],
+                'form' => ['is_enabled' => false],
+                'view' => ['is_displayable' => false],
+            ]
+        );
+
+        // Works in case when the affected field already exists.
+        $queries->addPostQuery(new UpdateEntityConfigFieldValueQuery(
+            Customer::class,
+            'external_id',
+            'datagrid',
+            'is_visible',
+            DatagridScope::IS_VISIBLE_FALSE
+        ));
+        $queries->addPostQuery(new UpdateEntityConfigFieldValueQuery(
+            Customer::class,
+            'external_id',
+            'form',
+            'is_enabled',
+            false
+        ));
+        $queries->addPostQuery(new UpdateEntityConfigFieldValueQuery(
+            Customer::class,
+            'external_id',
+            'view',
+            'is_displayable',
+            false
+        ));
+    }
+
+    private function hideCustomerUserExternalId(QueryBag $queries): void
+    {
+        // Works in case when the affected field does not yet exist.
+        $this->extendOptionsManager->mergeColumnOptions(
+            'oro_customer_user',
+            'external_id',
+            [
+                'datagrid' => ['is_visible' => DatagridScope::IS_VISIBLE_FALSE],
+                'form' => ['is_enabled' => false],
+                'view' => ['is_displayable' => false],
+            ]
+        );
+
+        // Works in case when the affected field already exists.
+        $queries->addPostQuery(new UpdateEntityConfigFieldValueQuery(
+            CustomerUser::class,
+            'external_id',
+            'datagrid',
+            'is_visible',
+            DatagridScope::IS_VISIBLE_FALSE
+        ));
+        $queries->addPostQuery(new UpdateEntityConfigFieldValueQuery(
+            CustomerUser::class,
+            'external_id',
+            'form',
+            'is_enabled',
+            false
+        ));
+        $queries->addPostQuery(new UpdateEntityConfigFieldValueQuery(
+            CustomerUser::class,
+            'external_id',
+            'view',
+            'is_displayable',
+            false
+        ));
+    }
+}
